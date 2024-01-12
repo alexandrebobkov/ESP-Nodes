47acaa51b536a2c6818476fadd4919ceacfb4eff
Alexandre B <alexandre.bobkov@gmail.com>
2024-01-12 02:10:43 -0500

    comments

--- ESP32-S2-Temperature-Node/src/main.cpp
+++ ESP32-S2-Temperature-Node/src/main.cpp
@@ -5,41 +5,47 @@
 #include "secrets.h"
 
 // Define tasks
-TaskHandle_t Status_Task;
-unsigned int t = 125;
+TaskHandle_t Status_Task; // Status LED task
+unsigned int t = 125;     // Blink delay time
 
-// put function declarations here:
+// Declare functions
 int myFunction(int, int);
 void StatusCode (void* parameters);
 
 void setup() {
   
+  // Initialize setup
   Serial.begin(115200);
   Serial.println("Running setip ...");
+  // Set status LED blink rapidly during setup phase
   t = 125;
+  // Start LED status task
   xTaskCreatePinnedToCore(StatusCode, "Status LED", 1000, NULL, 2, &Status_Task, 0);
 
+  // Initialize Wi-Fi connection
   WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   WiFi.mode(WIFI_STA);
   Serial.println("Connecting to Wi-Fi");  
-  // Connect to wifi.
+  // Connect to Wi-Fi
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print("#");
   }
+  // If Wi-Fi connection was successfull, then set status LED blink slowly
   delay(1500);
   t = 1500;
   Serial.print("\nCONNECTED\nIP: ");  
   Serial.println(WiFi.localIP());
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  //pinMode(LED_BUILTIN, OUTPUT);
+  //digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
 }
 
+// Status LED task code
 void StatusCode (void* parameters)
 {
   for (;;) {
