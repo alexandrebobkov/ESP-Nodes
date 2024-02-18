86aa04b4dd75286b3d8e986cd2f05714caa82bca
Alexandre B <alexandre.bobkov@gmail.com>
2024-02-18 00:41:55 -0500

    .

--- ESP32_ePaperDisplay-Node/src/main.cpp
+++ ESP32_ePaperDisplay-Node/src/main.cpp
@@ -1,12 +1,21 @@
 #include <Arduino.h>
+//#include <Fonts/FreeMonoBold9pt7b.h>
 #include "ePaper.h"
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  Serial.begin(115200);
+  Serial.println("Running setup ...");
+
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setCursor(10, 20);
+  display.print("ESP32 Display Node");
+  display.update();
 }
 
 void loop() {
