13977f683472032a71753cca1ac38df20e31b3f3
Alexandre B <alexandre.bobkov@gmail.com>
2024-02-18 09:42:33 -0500

    Status LED

--- ESP32_ePaperDisplay-Node/src/main.cpp
+++ ESP32_ePaperDisplay-Node/src/main.cpp
@@ -6,19 +6,28 @@
 int myFunction(int, int);
 
 void setup() {
+
+  pinMode(12, OUTPUT);
   
   Serial.begin(115200);
   Serial.println("Running setup ...");
 
+  /*display.init(115200);
   display.fillScreen(GxEPD_WHITE);
   display.setTextColor(GxEPD_BLACK);
   display.setFont(&FreeMonoBold9pt7b);
   display.setCursor(10, 20);
   display.print("ESP32 Display Node");
-  display.update();
+  display.update();*/
 }
 
 void loop() {
+  Serial.println("LED ON");
+  digitalWrite(12, HIGH);
+  delay(750);
+  Serial.println("LED OFF");
+  digitalWrite(12, LOW);
+  delay(750);
   // put your main code here, to run repeatedly:
 }
 
