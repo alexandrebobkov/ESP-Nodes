d201ed119c3267d4e80d4b57dd1f8925f2f505f2
Alexandre B <alexandre.bobkov@gmail.com>
2024-06-18 17:24:31 -0400

    Unified code to word with BMP280 and BME280 sensors

--- ESP32_Temperture-Node/src/config.h
+++ ESP32_Temperture-Node/src/config.h
@@ -1,12 +1,13 @@
 // Uncomment modules as required
 //#define RTC
 //#define MICRO_SD
-#define BMP280    // Adafruit BMP280; temp & pressure
-//#define BME280      // Generic I2C BME280; temp, pressure & humidity
+//#define BMP280    // Adafruit BMP280; temp & pressure
+#define BME280      // Generic I2C BME280; temp, pressure & humidity
 /*
     BME-280
     SCL -> GPIO 22
     SDA -> GPIO 21
+    VIN -> 5V
 */
 //#define AWSIoT
 
