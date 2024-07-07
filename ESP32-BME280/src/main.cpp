7d9c8d890ae1f8b0b241c49ecfe5d496a6400423
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-07 13:00:59 -0400

    Update main.cpp

--- ESP32-BME280/src/main.cpp
+++ ESP32-BME280/src/main.cpp
@@ -7,15 +7,12 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Running setup ...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
