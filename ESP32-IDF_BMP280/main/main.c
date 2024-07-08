fb8873a8704916fe3c0540a737b4c09da99bfc2a
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-08 18:25:32 -0400

    .

--- ESP32-IDF_BMP280/main/main.c
+++ ESP32-IDF_BMP280/main/main.c
@@ -103,6 +103,20 @@ static esp_err_t i2c_master_init(void)
     return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 }
 
+static esp_err_t i2c_driver_initialize(void)
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = 21,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = 22,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 100000,
+    };
+
+    return i2c_param_config(0, &conf);
+}
+
 void app_main(void)
 {
     i2c_driver_install(0, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
