4989f13a316fc0e427e347471afd8340f5ec1604
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-10 10:12:59 -0400

    .

---
+++ ESP32-IDF_BMP280/main/lm75.h
@@ -0,0 +1,14 @@
+#ifndef __LM75_H__
+#define __LM75_H__
+
+#define LM75_CHIP_ID                        (0x07)
+#define LM75_I2C_ADDRESS1                   (0x49)      // BME280 Data sheet p. 32
+#define BME280_I2C_ADDRESS2                 (0x49)
+#define BME280_TEMPERATURE_REGISTER         (0x00)
+
+#define BME280_TEMPERATURE_DATA_SIZE        (1)
+#define BME280_TEMPERATURE_DATA_LENGTH      (1)
+
+#endif
+
+
