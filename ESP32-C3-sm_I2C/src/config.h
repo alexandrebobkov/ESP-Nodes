db2044714e15cc6aee23a17802bb8a38452ec844
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-13 23:48:32 -0400

    .

---
+++ ESP32-C3-sm_I2C/src/config.h
@@ -0,0 +1,28 @@
+const char* WIFI_SSID = "IoT_bots";
+const char* WIFI_PASSWORD = "208208208";
+
+/*
+    ESP32-C3 Super Mini
+    SDA     -> GPIO6
+    SCL     -> GPIO7
+
+    SCK     -> GPIO8
+    MISO    -> GPIO9
+    MOSI    -> GPIO10
+
+    RX      -> GPIO20
+    TX      -> GPIO21
+
+    ADC1    -> GPIO1
+    ADC2    -> GPIO0
+
+    A3      -> GPIO5
+    A2      -> GPIO4
+    A1      -> GPIO3
+    A0      -> GPIO2
+    
+*/
+#define SDA_PIN (6)
+#define SCL_PIN (7)
+#define LED_PIN (4)
+
---
+++ ESP32-C3-sm_I2C/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <Adafruit_BME280.h>
+
+#include "config.h"
+
+Adafruit_BME280 bme;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Setting up GPIOs ...");
+
+  pinMode(LED_PIN, OUTPUT);
+
+  Serial.println("GPIO setup done");
+
+  Serial.println("Setting up BME280 sensor");
+  unsigned status = bme.begin(0x76); // 0x76
+  if (!status) {
+    Serial.println("Could not find a valid BME/BMP280 sensor, check wiring!");
+    Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+    Serial.print("   ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("   ID of 0x60 represents a BME 280.\n");
+    Serial.print("   ID of 0x61 represents a BME 680.\n");
+    while (1);
+  }
+}
+
+void loop() {
+  
+  digitalWrite(LED_PIN, HIGH);
+  delay(250);
+  digitalWrite(LED_PIN, LOW);
+  delay(250);
+  digitalWrite(LED_PIN, HIGH);
+  delay(250);
+  digitalWrite(LED_PIN, LOW);
+}
