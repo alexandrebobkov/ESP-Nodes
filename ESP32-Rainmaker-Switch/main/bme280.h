528e7c7cdc8c4bb17f893c307b52a69db7635685
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-17 11:48:15 -0400

    .

--- ESP32-Rainmaker-Switch/main/bme280.h
+++ ESP32-Rainmaker-Switch/main/bme280.h
@@ -32,6 +32,9 @@ typedef struct {
     float humidity;
     float pressure;
 } sensors;
+
+void init_bme280();
+
 static esp_err_t bme280_read_raw_temperature(uint8_t device_address, uint8_t *raw_temperature);
 static esp_err_t bme280_get_id(uint8_t device_address);
 
