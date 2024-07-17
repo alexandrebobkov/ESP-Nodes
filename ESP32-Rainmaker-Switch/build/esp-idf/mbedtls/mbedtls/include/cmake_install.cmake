ef4de6755e8be7be422b90ec66f897e9342934d9
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-16 22:21:50 -0400

    .

--- ESP32-Rainmaker-Switch/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake
+++ ESP32-Rainmaker-Switch/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/alex/.espressif/tools/tools/riscv32-esp-elf/esp-13.2.0_20230928/riscv32-esp-elf/bin/riscv32-esp-elf-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/alex/.espressif/tools/tools/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin/xtensa-esp32-elf-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
