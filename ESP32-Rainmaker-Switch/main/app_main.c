466d70fb776b27c0823b6d8a906d23df19ee0722
Alexandre B <alexandre.bobkov@gmail.com>
2024-07-18 01:55:49 -0400

    .

--- ESP32-Rainmaker-Switch/main/app_main.c
+++ ESP32-Rainmaker-Switch/main/app_main.c
@@ -191,7 +191,7 @@ void app_main()
         .enable_time_sync = false,
     };
     // Define device name and description as they should appear in logs
-    esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, "ESP RainMaker Device", "Switch C3");
+    esp_rmaker_node_t *node = esp_rmaker_node_init(&rainmaker_cfg, "ESP RainMaker Device", "Switch C3-2");
     if (!node) {
         ESP_LOGE(TAG, "Could not initialise node. Aborting!!!");
         vTaskDelay(5000/portTICK_PERIOD_MS);
@@ -203,7 +203,7 @@ void app_main()
      * avoid writing code for adding the name and power parameters.
      */
     // Define device name and description as they should appear in RainMaker UI app
-    switch_device = esp_rmaker_device_create("Switch C3", ESP_RMAKER_DEVICE_SWITCH, NULL);
+    switch_device = esp_rmaker_device_create("Switch C3-2", ESP_RMAKER_DEVICE_SWITCH, NULL);
 
     /* Add the write callback for the device. We aren't registering any read callback yet as
      * it is for future use.
@@ -214,7 +214,7 @@ void app_main()
      * user friendly custom name from the phone apps. All devices are recommended to have this
      * parameter.
      */
-    esp_rmaker_device_add_param(switch_device, esp_rmaker_name_param_create(ESP_RMAKER_DEF_NAME_PARAM, "Switch C3 Mini"));
+    esp_rmaker_device_add_param(switch_device, esp_rmaker_name_param_create(ESP_RMAKER_DEF_NAME_PARAM, "Switch C3-2 Mini"));
 
     /* Add the standard power parameter (type: esp.param.power), which adds a boolean param
      * with a toggle switch ui-type.
