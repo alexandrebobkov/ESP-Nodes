94396b86e430f7c96eed0fac378bcca9c1e5f782
Alexandre B <alexandre.bobkov@gmail.com>
2024-08-13 14:29:09 -0400

    .

--- ESP32-Rainmaker-Switch/main/app_driver.c
+++ ESP32-Rainmaker-Switch/main/app_driver.c
@@ -80,6 +80,9 @@ static void light_sensor_update(TimerHandle_t handle) {
     // Internal temperature    
     ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_value));
     ESP_LOGI(TAG, "\nESP32-C3 Module temperature: %0.2f", tsens_value);
+    esp_rmaker_param_update_and_report(
+        esp_rmaker_device_get_param_by_type(chip_sensor_device, ESP_RMAKER_PARAM_TEMPERATURE),
+        esp_rmaker_float(tsens_value));
 }
 void app_sensor_init(void) {
 //esp_err_t app_sensor_init(void) {
