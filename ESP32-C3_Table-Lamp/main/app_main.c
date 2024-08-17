b782d60bb0909021b77928b1ad5e6f2c096ce71a
Alexandre B <alexandre.bobkov@gmail.com>
2024-08-16 23:54:06 -0400

    .

--- ESP32-C3_Table-Lamp/main/app_main.c
+++ ESP32-C3_Table-Lamp/main/app_main.c
@@ -1,10 +1,10 @@
-/* Switch Example
+/* Table Night Ambient Lights
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+    Controlled by Rainmaker
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+    by: Alexander Bobkov
+
+    Date: August 10, 2024
 */
 
 #include <string.h>
