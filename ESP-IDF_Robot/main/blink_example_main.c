92bd47438c73a592c7d242f65cd69123636a8eb2
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-21 23:00:17 -0500

    ESP IDF Robot

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -1,10 +1,6 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/* Robot Controls
+    Generate PWM signals to control motors.
+    
 */
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
