3c863b242ecdc896eac737cbb101d4951dd57569
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-21 23:43:37 -0500

    ESP IDF Robot

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -127,6 +127,11 @@ void app_main(void)
 
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
     configure_button();
 
     while (1) {
