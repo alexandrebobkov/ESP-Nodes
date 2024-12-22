33baaac2cfa85b96fa5d29ab02c4dcb68628e3ef
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-21 23:36:51 -0500

    ESP IDF Robot

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -8,6 +8,8 @@
     build-in push button GPIO: 3
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
