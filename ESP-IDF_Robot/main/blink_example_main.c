3dfad28c93f6e67913a4b86f7dfd74506a421adc
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-21 23:56:42 -0500

    ESP IDF Robot

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -156,7 +156,8 @@ void app_main(void)
 
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
+        //blink_led();
+        gpio_set_level(BLINK_GPIO, s_led_state);
         /* Toggle the LED state */
         s_led_state = !s_led_state;
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
