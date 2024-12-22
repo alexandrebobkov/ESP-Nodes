910a7e3a235be72395c1a77ee1436efc831f3a82
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-22 09:53:38 -0500

    ESP IDF Robot

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -137,9 +137,19 @@ static void ledc_init (void) {
         .duty_resolution =  LEDC_DUTY_RES,
         .timer_num =        LEDC_TIMER,
         .freq_hz =          LEDC_FREQUENCY,
-        .clk_cfg =          LED_AUTO_CLK
+        .clk_cfg =          LEDC_AUTO_CLK
     };
     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode =       LEDC_MODE,
+        .channel =          LEDC_CHANNEL,
+        .timer_sel =        LEDC_TIMER,
+        .intr_type =        LEDC_INTR_DISABLE,
+        .gpio_num =         LEDC_OUTPUT_IO,
+        .duty =             0,
+        .hpoint =           0
+    };
 }
 
 void app_main(void)
