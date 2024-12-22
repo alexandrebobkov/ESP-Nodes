5f47c939b958b4cc039dcbcc7ce3dd6db5ac3d4f
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-22 14:03:50 -0500

    ESP IDF Robot

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -33,7 +33,7 @@ TIMER RESOLUTION    MAX VALUE   HALF-DUTY
 10                  1023            511
 13                  8191            4095
 */
-#define LEDC_DUTY               (7424) // 7680, 7424, 7168, 6144, 512, 768
+#define LEDC_DUTY               (7680) // 7680, 7424, 7168, 6144, 512, 768
 #define LEDC_FREQUENCY          (5000) // For LED the freuqncy of 500Hz seemed to be sufficient. // Frequency in Hertz. For DC motor, set frequency at 5 kHz
 
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
