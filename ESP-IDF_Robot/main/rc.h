0541863c8bbd86f163cabb18de799699b9ec948c
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 16:56:53 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -10,4 +10,12 @@ static int voltage[2][10];
 static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
 static void example_adc_calibration_deinit(adc_cali_handle_t handle);
 
+static esp_err_t rc_adc_init (void) {
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_CHNL,
+    };
+    return adc_oneshot_new_unit(&init_config1, &adc1_handle);
+}
+
 #endif
