0c6973f375d4fced3211d7c21241567ad02df86d
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 12:09:32 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -84,7 +84,7 @@ TIMER RESOLUTION    MAX VALUE   HALF-DUTY
 #define ADC_UNIT_STR(unit)          _ADC_UNIT_STR(unit)
 static TaskHandle_t s_task_handle;
 //static adc_channel_t channel[2] = {ADC_CHANNEL_2, ADC_CHANNEL_3};
-static adc_channel_t channel[2] = {ADC_CHANNEL_1, ADC_CHANNEL_2};
+static adc_channel_t channel[2] = {ADC_CHANNEL_1, ADC_CHANNEL_3};
 
 static int adc_raw[2][10];
 static int voltage[2][10];
