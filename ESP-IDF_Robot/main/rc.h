6e4e0d96813d445cf675d7fc5086cde1ca90125b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 16:50:23 -0500

    ESP-IDF Robot Controls

---
+++ ESP-IDF_Robot/main/rc.h
@@ -0,0 +1,9 @@
+#ifndef RC_H
+#define RC_H
+
+static int adc_raw[2][10];
+static int voltage[2][10];
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
+
+#endif
