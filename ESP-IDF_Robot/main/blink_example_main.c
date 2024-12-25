24b125f8ca30ef2d5da516023ed1a863303becb9
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 11:41:22 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -20,10 +20,10 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 /* ADC */
-#include "esp_adc/adc_oneshot.h"
+//#include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
-//#include "esp_adc/adc_continuous.h"
+#include "esp_adc/adc_continuous.h"
 
 /* ESP-NOW */
 #include <string.h>
