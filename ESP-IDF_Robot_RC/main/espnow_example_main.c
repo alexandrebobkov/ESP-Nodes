baf7d3b863b56e159621a20890c79043814d8e5c
Alexander B <alexandre.bobkov@gmail.com>
2024-12-25 01:39:16 -0500

    .

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -348,6 +348,7 @@ static esp_err_t example_espnow_init(void)
     send_param->unicast = false;
     send_param->broadcast = true;
     send_param->state = 0;
+    esp_random();
     send_param->magic = 50;//esp_random();
     send_param->count = CONFIG_ESPNOW_SEND_COUNT;
     send_param->delay = CONFIG_ESPNOW_SEND_DELAY;
