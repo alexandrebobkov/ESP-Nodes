be3995e2092393b2db43dd5d8d3e9dc2df1fd509
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 11:35:10 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -69,6 +69,7 @@ TIMER RESOLUTION    MAX VALUE   HALF-DUTY
 #define PUSH_BTN_GPIO   CONFIG_BUTTON_GPIO      // 3 GPIO of on-board push-button
 #define MTR_FL_GPIO     0 //CONFIG_MOTOR_FRONT_LEFT_GPIO
 // ADC
+#define ADC_UNIT    ADC_UNIT_1
 #define PROJ_X          (4)                     // ADC1_CH4; 4 GPIO joystick, x-axis
 #define PROJ_Y          (5)                     // ADC2_CH0; 5 GPIO joystick, y-axis
 #define NAV_BTN         (8)                     // 8 GPIO joystick button
