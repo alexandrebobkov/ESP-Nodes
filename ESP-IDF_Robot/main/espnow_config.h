3f641e112fbfbfa5d28c314b58cb7999b4d81096
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 03:28:56 -0500

    ESP-IDF Robot ESP NOW

--- ESP-IDF_Robot/main/espnow_config.h
+++ ESP-IDF_Robot/main/espnow_config.h
@@ -54,7 +54,7 @@ typedef struct {
     uint16_t seq_num;                     //Sequence number of ESPNOW data.
     uint16_t crc;                         //CRC16 value of ESPNOW data.
     uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
-    uint8_t payload[1];                   //Real payload of ESPNOW data.
+    uint8_t payload[2];                   //Real payload of ESPNOW data.
 } __attribute__((packed)) espnow_data_t;
 
 /* Parameters of sending ESPNOW data. */
