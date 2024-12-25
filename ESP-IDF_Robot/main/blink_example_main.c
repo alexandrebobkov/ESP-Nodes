f3f4462808f80fb454dd57656f5188bd60598ba8
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 03:10:38 -0500

    ESP-IDF Robot ESP NOW

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -314,7 +314,7 @@ int example_espnow_data_parse(uint8_t *data, uint16_t data_len, uint8_t *state, 
     crc_cal = esp_crc16_le(UINT16_MAX, (uint8_t const *)buf, data_len);
     // Display received data.
     ESP_LOGW(TAG, "Received data size: %d", data_len);
-    ESP_LOGW(TAG, "Received payload data is: %x", (uint8_t)buf->payload);
+    ESP_LOGW(TAG, "Saved value: %x", (uint8_t)buf->payload);
 
     if (crc_cal == crc) {
         return buf->type;
