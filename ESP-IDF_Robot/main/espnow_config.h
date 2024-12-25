4c1f1855b552674d19adbf9ee06d67eead0a7617
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-25 04:53:15 -0500

    ESP-IDF Robot ESP NOW

--- ESP-IDF_Robot/main/espnow_config.h
+++ ESP-IDF_Robot/main/espnow_config.h
@@ -62,6 +62,7 @@ typedef struct {
     bool unicast;                         //Send unicast ESPNOW data.
     bool broadcast;                       //Send broadcast ESPNOW data.
     uint8_t state;                        //Indicate that if has received broadcast ESPNOW data or not.
+    uint8_t priority;
     uint32_t magic;                       //Magic number which is used to determine which device to send unicast ESPNOW data.
     uint16_t count;                       //Total count of unicast ESPNOW data to be sent.
     uint16_t delay;                       //Delay between sending two ESPNOW data, unit: ms.
