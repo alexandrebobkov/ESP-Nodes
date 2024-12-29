33648ff70223ad7ce2e8eb3203983f1184d4654e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-29 00:23:53 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -894,7 +894,9 @@ void app_main(void)
 
         ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, m.motor2_rpm_pcm);
         ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-        ESP_LOGW(TAG, "Duty cycle: %d", var);
+        ESP_LOGW(TAG, "Motor 1 PWM: %d", var);
+        ESP_LOGW(TAG, "Interpolated value: %d", m.motor1_rpm_pcm);
+        ESP_LOGW(TAG, "Motor 2 PWM: %d", var);
         ESP_LOGW(TAG, "Interpolated value: %d", m.motor2_rpm_pcm);
     }
 
