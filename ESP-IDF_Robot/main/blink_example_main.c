5c2e133d38b3fd271bdac55bb4202d03a2bc224a
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-29 01:44:48 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -55,6 +55,7 @@ static const char *TAG = "ESP IDF Robot";
 // MOTORS
 #define MTR_FREQUENCY           (5000)
 #define MTR_MODE                LEDC_LOW_SPEED_MODE
+#define MTR_DUTY_RES           LEDC_TIMER_13_BIT
 #define MTR_FRONT_LEFT_IO       (6)
 #define MTR_FRONT_LEFT_TMR      LEDC_TIMER_0
 #define MTR_FRONT_LEFT          LEDC_CHANNEL_1
