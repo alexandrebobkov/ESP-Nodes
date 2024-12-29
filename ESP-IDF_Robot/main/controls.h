a7a3023d0c587979f4930c4a548d47209fbbe264
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-28 21:30:35 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/controls.h
+++ ESP-IDF_Robot/main/controls.h
@@ -1,11 +1,13 @@
 #ifndef CONTROLS_H
 #define CONTROLSC_H
 
-struct motors_pcm {
+struct motors_rpm {
     int motor1_rpm;
     int motor2_rpm;
     int motor3_rpm;
     int motor4_rpm;
 };
+
+struct motor {};
 
 #endif
