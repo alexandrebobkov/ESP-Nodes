70e097effcd3bf2ae371ca4740104b834bb2e2bd
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-28 21:37:30 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/controls.h
+++ ESP-IDF_Robot/main/controls.h
@@ -1,8 +1,12 @@
 #ifndef CONTROLS_H
 #define CONTROLSC_H
 
-// To prevent assigning forbidden rpm values to the motor (i.e. to avoid short-citcuit) accidentially,
-// we define one struct tha tholds RPMs for four motors, as opposed to defining array of structs for single motor. 
+/* 
+
+To prevent assigning forbidden rpm values to the motor (i.e. to avoid short-citcuit) accidentially,
+we define one struct tha tholds RPMs for four motors, as opposed to defining array of structs for single motor.
+
+*/ 
 struct motors_rpm {
     int motor1_rpm;
     int motor2_rpm;
