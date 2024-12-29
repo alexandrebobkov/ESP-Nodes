dc4544ed3b360761356e4e2e00f8a527dc660a8b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-29 02:10:45 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -71,6 +71,8 @@ static void rc_get_raw_data() {
         m.motor2_rpm_pcm = rescale_raw_val(adc_raw[0][0]);
     if (rescale_raw_val(adc_raw[0][1]) >= 4000)
         m.motor2_rpm_pcm = rescale_raw_val(adc_raw[0][1]);
+    
+    // Cut-off power if joystick is near-center
     if (rescale_raw_val(adc_raw[0][0]) < 4000)
         m.motor1_rpm_pcm = 0;
     if (rescale_raw_val(adc_raw[0][1]) < 4000)
