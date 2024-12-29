b08fb0b71367920baeb562a6b27cf07513c21fe5
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-28 19:08:02 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/motor_controls.h
+++ ESP-IDF_Robot/main/motor_controls.h
@@ -1,6 +1,11 @@
 #ifndef MOTOR_CONTROLS_H
 #define MOTOR_CONTROLSC_H
 
-static void interpolate_raw_measure () {}
+// Interpolate value (x) based on raw reading, min/max limits.
+static int interpolate_raw_measure (int min, int max, int raw) {
+    int x;
+
+    return x;
+}
 
 #endif
