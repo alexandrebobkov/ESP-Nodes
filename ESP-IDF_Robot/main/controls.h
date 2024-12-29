e86190b12978ca96a46907b62b915165389e75ac
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-28 22:36:25 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/controls.h
+++ ESP-IDF_Robot/main/controls.h
@@ -14,15 +14,15 @@ Struct that holds PCM for RPMs for each of 4 motors.
 Positive PCM values for clock-wise rotation, and negative values for counter-vise rotation.
 
 */
-typedef struct motors_rpm {
-    int* motor1_rpm_pcm;
-    int* motor1_gpio;
-    int* motor2_rpm_pcm;
-    int* motor2_gpio;
-    int* motor3_rpm_pcm;
-    int* motor3_gpio;
-    int* motor4_rpm_pcm;
-    int* motor4_gpio;
+struct motors_rpm {
+    int motor1_rpm_pcm;
+    int motor1_gpio;
+    int motor2_rpm_pcm;
+    int motor2_gpio;
+    int motor3_rpm_pcm;
+    int motor3_gpio;
+    int motor4_rpm_pcm;
+    int motor4_gpio;
 } Motors;
 
 void motors_init (void);
