8513125acaecd0e7d3aab1b817b5bc93ae8f93e4
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-29 02:10:29 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -71,7 +71,9 @@ static void rc_get_raw_data() {
         m.motor2_rpm_pcm = rescale_raw_val(adc_raw[0][0]);
     if (rescale_raw_val(adc_raw[0][1]) >= 4000)
         m.motor2_rpm_pcm = rescale_raw_val(adc_raw[0][1]);
-    else
+    if (rescale_raw_val(adc_raw[0][0]) < 4000)
+        m.motor1_rpm_pcm = 0;
+    if (rescale_raw_val(adc_raw[0][1]) < 4000)
         m.motor2_rpm_pcm = 0;
     /*
     if (rescale_raw_val(adc_raw[0][0]) >= 4000)
