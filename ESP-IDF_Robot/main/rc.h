d88aeba7750b09bd824378f5cfdb3787f3dc4bce
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-28 23:18:22 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -60,10 +60,10 @@ static void rc_get_raw_data() {
     ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC1_CHAN1, &adc_raw[0][1]));
     ESP_LOGI("ESP IDF Robot", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC1_CHAN0, adc_raw[0][0]);
     ESP_LOGI("ESP IDF Robot", "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, ADC1_CHAN1, adc_raw[0][1]);
-    ESP_LOGI("Joystick", "Position: %d", interpolate_raw_val(adc_raw[0][0]));
-    ESP_LOGI("Joystick", "Position: %d", interpolate_raw_val(adc_raw[0][1]));
-    m.motor1_rpm_pcm = interpolate_raw_val(adc_raw[0][0]);
-    m.motor2_rpm_pcm = interpolate_raw_val(adc_raw[0][1]);
+    ESP_LOGI("Joystick", "Position: %d", rescale_raw_val(adc_raw[0][0]));
+    ESP_LOGI("Joystick", "Position: %d", rescale_raw_val(adc_raw[0][1]));
+    m.motor1_rpm_pcm = rescale_raw_val(adc_raw[0][0]);
+    m.motor2_rpm_pcm = rescale_raw_val(adc_raw[0][1]);
     ESP_LOGI("PWM", "Motor 1 PWM: %d", m.motor1_rpm_pcm);
     ESP_LOGI("PWM", "Motor 2 PWM: %d", m.motor2_rpm_pcm);
 
