7e4a503815fbb17aa939c84ba45a4fbf9ae84775
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-31 06:11:08 -0500

    ESP-IDF RC Motors Controls

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -58,9 +58,9 @@ static esp_err_t rc_adc_init (void) {
 
 static void set_motor_pcm(int* motor , int duty) {
     if (duty >= 8191)
-        motor -> 8191;
+        *motor = 8191;
     else
-        motor -> duty;
+        *motor = duty;
 }
 
 static void rc_get_raw_data() {
