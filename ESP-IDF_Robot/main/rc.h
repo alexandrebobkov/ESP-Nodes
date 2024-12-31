7984ce03e7055fce5eef93a8eab4b1f173a0dc80
Alexander Bobkov <alexandre.bobkov@gmail.com>
2024-12-31 02:48:54 -0500

    ESP-IDF Robot Controls

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -66,7 +66,6 @@ static void rc_get_raw_data() {
     
     // Cut-off readings that have values less than 700, i.e. when joystick is centered
     if (rescale_raw_val(adc_raw[0][1]) < -500 && rescale_raw_val(adc_raw[0][0]) < 500) {
-        if (rescale_raw_val(adc_raw[0][0]) > 0)
             ESP_LOGW("RC", "REVERSE");
     }
     // FORWARD
