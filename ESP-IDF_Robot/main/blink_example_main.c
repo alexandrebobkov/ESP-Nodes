1ee600cdebff4a0fcb868cbc57246916fad782f6
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-01 17:11:39 -0500

    ESP-IDF RC Motors Controls

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -217,7 +217,7 @@ static void IRAM_ATTR gpio_isr_handler (void* arg) {
     uint32_t gpio_num = (uint32_t) arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
-
+// Push button interrupt task
 static void gpio_task (void* arg) {
     uint32_t io_num;
     for (;;) {
