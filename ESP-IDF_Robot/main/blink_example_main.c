c3a12acf43b0b62322a8ab8ee6d889b7b74e7561
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-02 02:15:41 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -742,6 +742,7 @@ static void led_task (void *pyParameter) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
 	    gpio_set_level(BLINK_GPIO, s_led_state);
 	    vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        s_led_state != s_led_state;
 	}
 }
 
