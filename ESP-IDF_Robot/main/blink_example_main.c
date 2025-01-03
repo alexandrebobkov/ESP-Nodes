ff4e0d2d91e669ec2cfc9e75073a312cbde75725
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 09:40:26 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -799,11 +799,11 @@ static void display_chip_temperature () {
 
 void onDataReceived (uint8_t *mac_addr, uint8_t *data, uint8_t data_len) {
 
-    memcpy(buf, data, data_len);
-    //buf = (sensors_data_t*)data;
+    //memcpy(buf, data, data_len);
+    buf = (sensors_data_t*)data;
     ESP_LOGW(TAG, "Data was received");
-    ESP_LOGI(TAG, "x-axis: 0x%04X", (uint8_t)buf->x_axis);
-    ESP_LOGI(TAG, "y-axis: 0x%04X", (uint8_t)buf->y_axis);
+    ESP_LOGI(TAG, "x-axis: 0x%04X", buf->x_axis);
+    ESP_LOGI(TAG, "y-axis: 0x%04X", buf->y_axis);
     ESP_LOGI(TAG, "PCM 1: 0x%04X", buf->motor1_rpm_pcm);
     
 }
