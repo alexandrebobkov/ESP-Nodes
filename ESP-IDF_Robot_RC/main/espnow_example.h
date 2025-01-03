b37e4a161bb4060c2c739135d4ea54a023249794
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 01:03:39 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example.h
+++ ESP-IDF_Robot_RC/main/espnow_example.h
@@ -66,6 +66,15 @@ typedef struct {
     uint8_t payload[2];                   // Real payload of ESPNOW data.
 } __attribute__((packed)) example_espnow_data_t;
 
+typedef struct {
+    uint8_t type;                         // Broadcast or unicast ESPNOW data.
+    uint8_t state;                        // Indicate that if has received broadcast ESPNOW data or not.
+    uint16_t seq_num;                     // Sequence number of ESPNOW data.
+    uint16_t crc;                         // CRC16 value of ESPNOW data.
+    uint32_t magic;                       // Magic number which is used to determine which device to send unicast ESPNOW data.
+    uint8_t payload[2];                   // Real payload of ESPNOW data.
+} __attribute__((packed)) sensors_data_t;
+
 /* Parameters of sending ESPNOW data. */
 typedef struct {
     bool unicast;                         // Send unicast ESPNOW data.
