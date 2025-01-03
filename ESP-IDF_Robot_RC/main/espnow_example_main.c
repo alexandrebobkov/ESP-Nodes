cdd4baa3c7d130ba502b52c423d0563dbbb8b5ec
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 09:15:48 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -466,10 +466,10 @@ void sendData (void) {
     buffer = malloc(sizeof(sensors_data_t));
     buffer->type = 1;
     buffer->crc = 0;
-    buffer->x_axis = 0;
-    buffer->y_axis = 0;
+    buffer->x_axis = 240;
+    buffer->y_axis = 2040;
     buffer->nav_bttn = 0;
-    buffer->motor1_rpm_pcm = 0;
+    buffer->motor1_rpm_pcm = 10;
     buffer->motor2_rpm_pcm = 0;
     buffer->motor3_rpm_pcm = 0;
     buffer->motor4_rpm_pcm = 0;
