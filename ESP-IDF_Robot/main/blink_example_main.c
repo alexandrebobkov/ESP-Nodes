db88a6a3351f8923132cbc84c0b5bd1cb76609fe
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 09:56:35 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -802,10 +802,15 @@ void onDataReceived (uint8_t *mac_addr, uint8_t *data, uint8_t data_len) {
     //memcpy(buf, data, data_len);
     memcpy(&buf, data, sizeof(buf));
     //buf = (sensors_data_t*)data;
-    ESP_LOGW(TAG, "Data was received");
+    /*ESP_LOGW(TAG, "Data was received");
     ESP_LOGI(TAG, "x-axis: 0x%04X", buf->x_axis);
     ESP_LOGI(TAG, "y-axis: 0x%04X", buf->y_axis);
-    ESP_LOGI(TAG, "PCM 1: 0x%04X", buf->motor1_rpm_pcm);
+    ESP_LOGI(TAG, "PCM 1: 0x%04X", buf->motor1_rpm_pcm);*/
+
+    ESP_LOGW(TAG, "Data was received");
+    ESP_LOGI(TAG, "x-axis: 0x%04X", buf.x_axis);
+    ESP_LOGI(TAG, "y-axis: 0x%04X", buf.y_axis);
+    ESP_LOGI(TAG, "PCM 1: 0x%04X", buf.motor1_rpm_pcm);
     
 }
 
