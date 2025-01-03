9498dc4cbe2ed017ca981624beaad90260fb6414
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 03:38:22 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -485,8 +485,9 @@ static void rc_send_data_task2 (void *pvParameter) {
 
     while (true) {
         memcpy(send_packet->dest_mac, receiver_mac, ESP_NOW_ETH_ALEN);
-        if (esp_now_send(send_packet->dest_mac, send_packet->buffer, send_packet->len) != ESP_OK) {
-            ESP_LOGE(TAG, "Send error");
+        esp_err_t r = esp_now_send(send_packet->dest_mac, send_packet->buffer, send_packet->len);
+        if (r != ESP_OK) {
+            ESP_LOGE(TAG, "Send error, %i", r);
             vTaskDelete(NULL);
             break;
         }
