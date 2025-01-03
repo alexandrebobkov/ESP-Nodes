6541ab7da1598b656155a4ab0d2b6f4ef20562c0
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 02:00:21 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -42,9 +42,9 @@
             ESP NOW
     ============================
 
-    ESP32-C3 Luatos ESP32C3 board MAC:    54:32:04:46:71:80
-    ESP32-C3 SuperMini MAC:     34:b7:da:f9:33:8d
-    ESP32-C3 Breadboard MAC:    e4:b0:63:17:9e:45
+    ESP32-C3 Luatos ESP32C3 board MAC:      54:32:04:46:71:80
+    ESP32-C3 SuperMini MAC:                 34:b7:da:f9:33:8d
+    ESP32-C3 Breadboard MAC:                e4:b0:63:17:9e:45
 */
 
 static uint8_t receiver_mac[ESP_NOW_ETH_ALEN]   = {0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45};
