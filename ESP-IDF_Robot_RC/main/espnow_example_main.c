062f3e708fbdc0a8641bf450a8637e607bb093ca
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 00:58:28 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -38,6 +38,17 @@
 #define PROJ_Y                      (0)                     // ADC1_CH0; 1 GPIO joystick, y-axis
 #define NAV_BTN                     (8)                     // 8 GPIO joystick button
 
+/*  ============================
+            ESP NOW
+    ============================
+
+    ESP32-C3 Blue board MAC:    54:32:04:46:71:80
+    ESP32-C3 SuperMini MAC:     34:b7:da:f9:33:8d
+    ESP32-C3 Breadboard MAC:    e4:b0:63:17:9e:45
+*/
+static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN]  = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+static uint8_t robot_mac[ESP_NOW_ETH_ALEN]      = {0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45};     // MAC address of Robot
+static uint8_t rc_mac[ESP_NOW_ETH_ALEN]         = {0x34, 0xB7, 0xDA, 0xF9, 0x33, 0x8D};     // MAC address of Remote Control
 
 #define ESPNOW_MAXDELAY 512
 
