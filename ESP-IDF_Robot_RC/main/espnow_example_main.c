3fa8c2ba00a1fb62e5bfebb9d40a8cf448622a03
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 00:39:41 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -35,6 +35,7 @@
 #define PROJ_X                      (1)                     // ADC1_CH1; 0 GPIO joystick, x-axis
 #define PROJ_Y                      (0)                     // ADC1_CH0; 1 GPIO joystick, y-axis
 #define NAV_BTN                     (8)                     // 8 GPIO joystick button
+struct motors_rpm m;
 
 
 #define ESPNOW_MAXDELAY 512
