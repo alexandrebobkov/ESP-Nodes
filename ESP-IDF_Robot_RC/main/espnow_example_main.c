dbf6e3e77bbce792ba73004bbc24678997e85523
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 03:45:50 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -505,8 +505,7 @@ static esp_err_t rc_espnow_init (void) {
     }
 
     memset(send_packet, 0, sizeof(espnow_data_packet_t));
-    //memcpy(send_packet->dest_mac, receiver_mac, ESP_NOW_ETH_ALEN);
-    send_packet->dest_mac = receiver_mac;
+    memcpy(send_packet->dest_mac, receiver_mac, ESP_NOW_ETH_ALEN);
     send_packet->len = CONFIG_ESPNOW_SEND_LEN; // 128
     send_packet->buffer = malloc(CONFIG_ESPNOW_SEND_LEN);
     sensors_data_prepare(send_packet);
