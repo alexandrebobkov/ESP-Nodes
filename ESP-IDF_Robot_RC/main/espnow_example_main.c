01966a3913f9b9bdc387c7661144d9a32a0213c4
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 09:30:09 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -543,7 +543,9 @@ void onDataReceived (uint8_t *mac_addr, uint8_t *data, uint8_t data_len) {
     //memcpy(buf, data, data_len);
     buf = (sensors_data_t*)data;
     ESP_LOGW(TAG, "Data was received");
-    ESP_LOGI(TAG, "x-axis: %i", buf->x_axis);
+    ESP_LOGI(TAG, "x-axis: 0x%04x", buf->x_axis);
+    ESP_LOGI(TAG, "x-axis: 0x%04x", buf->y_axis);
+    ESP_LOGI(TAG, "PCM 1: 0x%04x", buf->motor1_rpm_pcm);
 }
 
 
