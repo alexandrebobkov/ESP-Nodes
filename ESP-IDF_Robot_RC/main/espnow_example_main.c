5974bbd6e845b5ffa8432b215d60d82f801385df
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 02:57:54 -0500

    ESP32-C3 RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -486,6 +486,7 @@ static esp_err_t rc_espnow_init (void) {
     memset(send_packet, 0, sizeof(espnow_data_packet_t));
     send_packet->dest_mac = receiver_mac;
     send_packet->len = CONFIG_ESPNOW_SEND_LEN; // 128
+    send_packet->buffer = malloc(CONFIG_ESPNOW_SEND_LEN);
 
     xTaskCreate(example_espnow_task, "controller data packets task", 2048, send_packet, 14, NULL);
 
