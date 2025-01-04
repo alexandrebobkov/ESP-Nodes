e9e327d13a50eb29a493992fc39a6d961faa01a4
Alexander B <alexandre.bobkov@gmail.com>
2025-01-03 19:21:53 -0500

    .

--- ESP-IDF_Robot_RC/main/config.h
+++ ESP-IDF_Robot_RC/main/config.h
@@ -5,4 +5,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/*
+    Joystick
+*/
+#define PROJ_X                      (1)                     // ADC1_CH1; 0 GPIO joystick, x-axis
+#define PROJ_Y                      (0)                     // ADC1_CH0; 1 GPIO joystick, y-axis
+#define NAV_BTN                     (8)                     // 8 GPIO joystick button
+
 #endif
