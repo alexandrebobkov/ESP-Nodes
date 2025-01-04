b07f4e4c0b4d1f457c29408d790d9d95dc668c55
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-04 02:34:17 -0500

    ESP32-C3 RC & Receiver

--- ESP-IDF_Robot_RC/main/receiver.c
+++ ESP-IDF_Robot_RC/main/receiver.c
@@ -14,3 +14,17 @@ void onDataReceived (uint8_t *mac_addr, uint8_t *data, uint8_t data_len) {
     ESP_LOGI(TAG, "x-axis: 0x%04x", buf->y_axis);
     ESP_LOGI(TAG, "PCM 1: 0x%04x", buf->motor1_rpm_pcm);*/
 }
+
+static void rc_task (void *arg) {
+    while (true) {
+        rc_get_raw_data();
+
+        ESP_LOGI("PWM", "Motor 1 PWM: %d", m.motor1_rpm_pcm);
+        ESP_LOGI("PWM", "Motor 2 PWM: %d", m.motor2_rpm_pcm);
+        ESP_LOGI("PWM", "Motor 3 PWM: %d", m.motor3_rpm_pcm);
+        ESP_LOGI("PWM", "Motor 4 PWM: %d", m.motor4_rpm_pcm);
+
+        //vTaskDelay (10 / portTICK_PERIOD_MS);  // Determines responsiveness  
+        vTaskDelay (1000 / portTICK_PERIOD_MS); 
+    }
+}
