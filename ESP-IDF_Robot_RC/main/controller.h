efb46a73dd4d6b4943d4eb1ffa83367c9a5ab92c
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-04 02:15:56 -0500

    ESP32-C3 RC & Receiver

--- ESP-IDF_Robot_RC/main/controller.h
+++ ESP-IDF_Robot_RC/main/controller.h
@@ -5,6 +5,8 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include "common.h"
+
 void onDataSent (uint8_t *mac_addr, esp_now_send_status_t status);
 void sensors_data_prepare(espnow_data_packet_t *send_packet);
 static void rc_send_data_task (void *arg);
