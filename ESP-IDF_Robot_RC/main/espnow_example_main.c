93688e6d761b3a01e64e61279b5fa2b7c3c92948
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-03 23:56:57 -0500

    ESP32-C3 RC & Receiver

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -65,7 +65,7 @@ static QueueHandle_t s_example_espnow_queue;
 static uint8_t s_example_broadcast_mac[ESP_NOW_ETH_ALEN] = { 0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45 };
 static uint16_t s_example_espnow_seq[EXAMPLE_ESPNOW_DATA_MAX] = { 0, 0 };
 
-static void example_espnow_deinit(example_espnow_send_param_t *send_param);
+
 
 /* WiFi should start before using ESPNOW */
 static void wifi_init(void)
