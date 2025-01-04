5b70f31fc41308d35542dffcc6e25749d8e6639b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-04 02:16:47 -0500

    ESP32-C3 RC & Receiver

--- ESP-IDF_Robot_RC/main/receiver.c
+++ ESP-IDF_Robot_RC/main/receiver.c
@@ -6,9 +6,9 @@ extern static sensors_data_t *buffer;
 void onDataReceived (uint8_t *mac_addr, uint8_t *data, uint8_t data_len) {
 
     //memcpy(buf, data, data_len);
-    buf = (sensors_data_t*)data;
+    /*buf = (sensors_data_t*)data;
     ESP_LOGW(TAG, "Data was received");
     ESP_LOGI(TAG, "x-axis: 0x%04x", buf->x_axis);
     ESP_LOGI(TAG, "x-axis: 0x%04x", buf->y_axis);
-    ESP_LOGI(TAG, "PCM 1: 0x%04x", buf->motor1_rpm_pcm);
+    ESP_LOGI(TAG, "PCM 1: 0x%04x", buf->motor1_rpm_pcm);*/
 }
