d3fd0f3a2c04b7c4057b54680390612c7e9db790
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-04 03:21:18 -0500

    ESP32-C3 RC & Receiver

--- ESP-IDF_Robot_RC/main/controller.c
+++ ESP-IDF_Robot_RC/main/controller.c
@@ -89,6 +89,25 @@ void sendData (void) {
     else
         ESP_LOGW("ESP-NOW", "Data was sent.");
 }
+static esp_err_t rc_espnow_init (void) {
+
+    espnow_data_packet_t *send_packet;
+
+    send_packet = malloc(sizeof(espnow_data_packet_t));
+    if (send_packet == NULL) {
+        ESP_LOGE(TAG, "malloc fail.");
+        return ESP_FAIL;
+    }
+
+    memset(send_packet, 0, sizeof(espnow_data_packet_t));
+    memcpy(send_packet->dest_mac, receiver_mac, ESP_NOW_ETH_ALEN);
+    send_packet->len = CONFIG_ESPNOW_SEND_LEN; // 128
+    send_packet->buffer = malloc(CONFIG_ESPNOW_SEND_LEN);
+    sensors_data_prepare(send_packet);
+    xTaskCreate(rc_send_data_task2, "controller data packets task", 2048, send_packet, 8, NULL);
+
+    return ESP_OK;
+}
 
 static void rc_send_data_task (void *arg) {
 
