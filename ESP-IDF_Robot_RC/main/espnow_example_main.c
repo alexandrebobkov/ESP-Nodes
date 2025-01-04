428f0f45ec4c1a60106a2e2381922f170c53b991
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-04 05:06:37 -0500

    ESP32-C3 RC & Receiver

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -105,27 +105,9 @@ void sendData (void) {
     ESP_LOGI(TAG, "pcm 3: 0x%04X", (uint8_t)buffer.motor3_rpm_pcm);
     ESP_LOGI(TAG, "pcm 4: 0x%04X", (uint8_t)buffer.motor4_rpm_pcm);
 
-    /*sensors_data_t *buffer;
-    buffer = malloc(sizeof(sensors_data_t));
-    buffer->type = 1;
-    buffer->crc = 0;
-    buffer->x_axis = 240;
-    buffer->y_axis = 256;
-    buffer->nav_bttn = 0;
-    buffer->motor1_rpm_pcm = 10;
-    buffer->motor2_rpm_pcm = 0;
-    buffer->motor3_rpm_pcm = 0;
-    buffer->motor4_rpm_pcm = 0;
-    ESP_LOGI(TAG, "x-axis: 0x%04X", (uint8_t)buffer->x_axis);
-    ESP_LOGI(TAG, "y-axis: 0x%04X", (uint8_t)buffer->y_axis);
-    ESP_LOGI(TAG, "pcm 1: 0x%04X", (uint8_t)buffer->motor1_rpm_pcm);
-    ESP_LOGI(TAG, "pcm 2: 0x%04X", (uint8_t)buffer->motor2_rpm_pcm);
-    ESP_LOGI(TAG, "pcm 3: 0x%04X", (uint8_t)buffer->motor3_rpm_pcm);
-    ESP_LOGI(TAG, "pcm 4: 0x%04X", (uint8_t)buffer->motor4_rpm_pcm);*/
-
     //uint8_t result = esp_now_send(receiver_mac, &flagToSend, sizeof(flagToSend));
     uint8_t result = esp_now_send(receiver_mac, &buffer, sizeof(buffer));
-    //uint8_t result = esp_now_send(receiver_mac, (sensors_data_t *)&buffer, sizeof(buffer));
+
     if (result != 0) {
         ESP_LOGE("ESP-NOW", "Error sending data! Error code: 0x%04X", result);
         deletePeer();
