cd0a4af436d62c2b9282fd968a504f6413692499
Alexander B <alexandre.bobkov@gmail.com>
2025-01-05 01:37:48 -0500

    RC

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -104,12 +104,13 @@ static void update_pwm (int rc_x, int rc_y) {
             m.motor3_rpm_pcm = -x;
             m.motor4_rpm_pcm = -x;
         }
-        else if ((y < 0 && y > -200) && (x < -1000)) {
+        else if ((x > 0 && x < 500) &&(y < 200)) {
+        //else if ((y < 0 && y > -200) && (x < -1000)) {
             ESP_LOGW("ESP-NOW", "LEFT");
             // Left side rotates in forward direction, right side rotates in reverse direction.
-            m.motor1_rpm_pcm = -x;
+            m.motor1_rpm_pcm = -y;
             m.motor2_rpm_pcm = 0;
-            m.motor3_rpm_pcm = -x;
+            m.motor3_rpm_pcm = -y;
             m.motor4_rpm_pcm = 0;
         }
         else if ((y < 0 && y > -200) && (x > 1000)) {
