12639ef8f8dc15c944137d7d73c91b5770b8ff32
Alexander B <alexandre.bobkov@gmail.com>
2025-01-04 22:48:52 -0500

    RC

--- ESP-IDF_Robot_RC/main/joystick.h
+++ ESP-IDF_Robot_RC/main/joystick.h
@@ -8,8 +8,7 @@
 #include "esp_adc/adc_oneshot.h"
 
 static int x, y;
-static adc_oneshot_unit_handle_t adc_xy_handle;
-extern static sensors_data_t buffer;   
+static adc_oneshot_unit_handle_t adc_xy_handle; 
 
 static esp_err_t joystick_adc_init() {
     adc_oneshot_unit_init_cfg_t adc_init_config_xy = {
@@ -33,13 +32,10 @@ static esp_err_t joystick_adc_init() {
     return ESP_OK;
 }
 
-static void joystick_get_raw_xy() {
+static void joystick_show_raw_xy() {
 
     ESP_ERROR_CHECK(adc_oneshot_read(adc_xy_handle, ADC1_CHANNEL_0, &x));
     ESP_ERROR_CHECK(adc_oneshot_read(adc_xy_handle, ADC1_CHANNEL_1, &y));
-
-    buffer.x_axis = &x;
-    buffer.y_axis = &y;
 
     ESP_LOGI("(x,y)", "( %d, %d )", x, y);
 }
@@ -47,7 +43,7 @@ static void joystick_get_raw_xy() {
 static void joystick_task(void *arg) {
 
     while (true) {
-        joystick_get_raw_xy();
+        joystick_show_raw_xy();
         vTaskDelay (750 / portTICK_PERIOD_MS);
     }
 }
