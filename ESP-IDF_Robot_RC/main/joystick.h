0af937bb2c165ef27805282b9da6929657b4a7d2
Alexander B <alexandre.bobkov@gmail.com>
2025-01-04 21:53:24 -0500

    RC

--- ESP-IDF_Robot_RC/main/joystick.h
+++ ESP-IDF_Robot_RC/main/joystick.h
@@ -8,7 +8,7 @@ static unsigned uint8_t x = 0, y = 0;
 adc_oneshot_unit_handle_t adc1_x_handle, adc1_y_handle;
 
 static esp_err_t joystick_adc_init() {
-    adc_oneshot_unit_init_cgf_t adc_init_config1 = {
+    adc_oneshot_unit_init_cfg_t adc_init_config1 = {
         .unit_id = ADC_UNIT_1,
     };
     ESP_ERROR_CHECK(adc_oneshot_new_init(&adc_init_config1, &adc1_x_handle));
