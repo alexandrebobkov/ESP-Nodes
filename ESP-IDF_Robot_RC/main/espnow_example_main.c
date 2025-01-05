a74c16128befcfb2c15674e0bf560b1d6d7808fe
Alexander B <alexandre.bobkov@gmail.com>
2025-01-04 22:10:17 -0500

    RC

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -126,8 +126,9 @@ static void rc_send_data_task (void *arg) {
 
 void app_main(void)
 {
-    if (ESP_ERROR_CHECK(joystick_adc_init() == 0)
+    if (ESP_ERROR_CHECK(joystick_adc_init() == 0) {
         xTaskCreate(joystick_task, "RC", 2048, NULL, 5, NULL);
+    }
 
     // Initialize NVS to store Wi-Fi configurations
     esp_err_t ret = nvs_flash_init();
