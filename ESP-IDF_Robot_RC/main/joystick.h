b5f7022726ab755f9b9c39dbc505b026ee1105fb
Alexander B <alexandre.bobkov@gmail.com>
2025-01-04 21:23:09 -0500

    RC

--- ESP-IDF_Robot_RC/main/joystick.h
+++ ESP-IDF_Robot_RC/main/joystick.h
@@ -1,6 +1,11 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
 
-static void joystick_get_raw_xy() {}
+static unsigned uint8_t x, y;
+adc_oneshot_unit_handle_t adc1_x_handle;
+
+static void joystick_get_raw_xy() {
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC1_CHAN0, &x));
+}
 
 #endif
