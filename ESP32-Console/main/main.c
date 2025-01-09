653fd5c84c355930af5182cf1e0e5dc5ece50d2c
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-09 17:24:02 -0500

    ESP32-Console

--- ESP32-Console/main/main.c
+++ ESP32-Console/main/main.c
@@ -6,8 +6,15 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
+#include "esp_log.h"
 #include "esp_console.h"
+
+static void initialize_nvs (void) {
+    esp_err_t err = nvs_flash_init();
+    if (err = ESP_ERR_NVS_NO_FREE_PAGES || err = ESP_ERR_NVS_NEW_VERSION) 
+}
 
 void app_main(void)
 {
