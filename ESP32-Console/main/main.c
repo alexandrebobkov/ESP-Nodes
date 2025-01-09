bf4a13dbdcc00e32e76cbec13ccdaf94e3597efb
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-09 17:28:57 -0500

    ESP32-Console

--- ESP32-Console/main/main.c
+++ ESP32-Console/main/main.c
@@ -19,7 +19,7 @@
 
 static void initialize_nvs (void) {
     esp_err_t err = nvs_flash_init();
-    if (err = ESP_ERR_NVS_NO_FREE_PAGES || err = ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK (nvs_flash_erase());
         err = nvs_flash_init();
     }
