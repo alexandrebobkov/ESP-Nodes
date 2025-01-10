b8b1ab9588f149c59e49fbdbb8320420b0529da4
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-10 17:28:09 -0500

    ESP32-Console

--- ESP32-Console/main/commands.h
+++ ESP32-Console/main/commands.h
@@ -1,10 +1,14 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+static struct {
+    struct arg_int *detail;
+    struct arg_int *temp;
+    struct arg_int *voltage;
+} info_args;
 static int do_info_cmd (int argc, char **argv) {
     return 0;
 }
-
 static void register_info (void) {
     const esp_console_cmd_t info_cmd = {
         .command = "info",
