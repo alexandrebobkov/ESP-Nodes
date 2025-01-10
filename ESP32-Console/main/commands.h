8848c27b00ef15c34f3bbd95dac97a48f96642bd
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-10 17:26:53 -0500

    ESP32-Console

--- ESP32-Console/main/commands.h
+++ ESP32-Console/main/commands.h
@@ -1,4 +1,18 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+static int do_info_cmd (int argc, char **argv) {
+    return 0;
+}
+
+static void register_info (void) {
+    const esp_console_cmd_t info_cmd = {
+        .command = "info",
+        .help = "System Information",
+        .hint = NULL,
+        .func = &do_info_cmd,
+        .argtable = &info_args
+    };
+}
+
 #endif
