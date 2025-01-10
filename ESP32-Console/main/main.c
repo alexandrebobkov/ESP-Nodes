2358a0c2a2a0ffdb2c3324a0d5b5ff25b3d1d732
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-10 17:52:50 -0500

    ESP32-Console

--- ESP32-Console/main/main.c
+++ ESP32-Console/main/main.c
@@ -37,8 +37,11 @@ void app_main(void)
 {
     esp_console_repl_t *repl = NULL;
     esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    esp_console_dev_uart_config_t uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
 
     initialize_nvs();
+
+    esp_console_new_repl_uart(&uart_config, &repl_config, &repl));
 
     repl_config.prompt = "foxie >";
 
