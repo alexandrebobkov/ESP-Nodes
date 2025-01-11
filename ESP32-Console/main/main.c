ab9f82d68e27f4c566eee64ab9f4201149abea8e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-11 09:20:15 -0500

    ESP32-Console

--- ESP32-Console/main/main.c
+++ ESP32-Console/main/main.c
@@ -36,7 +36,7 @@ static void initialize_filesystem(void)
     };
     esp_err_t err = esp_vfs_fat_spiflash_mount_rw_wl(MOUNT_PATH, "storage", &mount_config, &wl_handle);
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to mount FATFS (%s)", esp_err_to_name(err));
+        ESP_LOGE("Console", "Failed to mount FATFS (%s)", esp_err_to_name(err));
         return;
     }
 }
