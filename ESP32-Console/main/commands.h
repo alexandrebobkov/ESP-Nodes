ab8d87b5bef58482a68ec8bb59842b82d6819fd6
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-11 10:24:17 -0500

    ESP32-Console

--- ESP32-Console/main/commands.h
+++ ESP32-Console/main/commands.h
@@ -29,12 +29,12 @@ static int do_info_cmd (int argc, char **argv) {
     return 0;
 }
 static void register_info (void) {
-    info_args.detail = arg_str1(NULL, NULL, "<temp>", "Chip temperature");
+    info_args.detail = arg_str1(NULL, NULL, "<temp>", "Display chip temperature");
     info_args.end = arg_end(2);
 
     const esp_console_cmd_t info_cmd = {
         .command = "info",
-        .help = "System Information",
+        .help = "Prints system information",
         .hint = NULL,
         .func = &do_info_cmd,
         .argtable = &info_args
