19233496451f2789c5d4201b152c886c6e0ece3f
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-13 09:25:18 -0500

    ESP32-Console

--- ESP32-Console/main/commands.h
+++ ESP32-Console/main/commands.h
@@ -49,7 +49,7 @@ static int exec_info_cmd (int argc, char **argv) {
 }
 static void register_info (void) {
     info_args.temp = arg_str0("t", "temp", "<C|F>", "Diplays the chip temperature.");
-    info_args.voltage = arg_str0("v", "voltage", "<V|mV>", "Display voltage.");
+    info_args.voltage = arg_str0("v", "voltage", "<V|mV>", "Displays voltage.");
     //info_args.temp = arg_litn("t", "temp", 0, 1, "chip temperature");
     //info_args.detail = arg_str1(NULL, "gpio", "<num>", "Diplays the GPIO level.");
     info_args.end = arg_end(2);
