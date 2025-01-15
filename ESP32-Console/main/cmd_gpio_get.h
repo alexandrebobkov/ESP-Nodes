bd1a98b5300a706339029d37daf4d4cff884c904
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-15 17:08:41 -0500

    ESP32-Console

--- ESP32-Console/main/cmd_gpio_get.h
+++ ESP32-Console/main/cmd_gpio_get.h
@@ -31,8 +31,8 @@ static int exec_gpio_get_cmd(int argc, char **argv) {
     }
     else {
         if (gpio_get_args.gpio != 0) {
-            //int *pin = gpio_get_args.gpio->ival;
-            //printf("GPIO: %d \n", *pin);
+            int pin = gpio_get_args.gpio->ival;
+            printf("GPIO: %d \n", pin);
             ESP_LOGI("gpio-get", "GPIO # %i", gpio_get_args.gpio->ival[0]);
             gpio_dump_io_configuration(stdout, 1ULL << 4);//gpio_get_args.gpio->ival[0]);
             s = 0;
@@ -43,7 +43,7 @@ static int exec_gpio_get_cmd(int argc, char **argv) {
 
 static void register_gpio_get_cmd (void) {
 
-    gpio_get_args.gpio      = arg_int0("p", "gpio", "<pin>", "Specifies GPIO to be used.");
+    gpio_get_args.gpio      = arg_int0("p", "pin", "<pin>", "Specifies GPIO to be used.");
     gpio_get_args.end       = arg_end(2);
     const esp_console_cmd_t gpio_get_cmd = {
         .command    = "gpio-get",
