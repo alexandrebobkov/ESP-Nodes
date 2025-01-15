e479b539307856158fcbf39b6562a0d9a8ece1fb
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-15 10:35:34 -0500

    ESP32-Console

--- ESP32-Console/main/cmd_gpio_get.h
+++ ESP32-Console/main/cmd_gpio_get.h
@@ -8,6 +8,11 @@
 #include "esp_console.h"
 #include "esp_log.h"
 
+static struct {
+    struct arg_int *gpio;
+    struct arg_end *end;
+} gpio_get_args;
+
 // Operate GPIOs states
 static int exec_gpio_get_cmd(int argc, char **argv);
 static void register_gpio_get_cmd(void);
