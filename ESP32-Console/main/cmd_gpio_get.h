9bd83437cc687d372d31ad223e77ea408122b2bf
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-17 18:27:45 -0500

    ESP32-Console

--- ESP32-Console/main/cmd_gpio_get.h
+++ ESP32-Console/main/cmd_gpio_get.h
@@ -36,7 +36,7 @@ static int exec_gpio_get_cmd(int argc, char **argv) {
         // Gets GPIO level
         if (gpio_get_args.gpio->count > 0) {
             pin = gpio_get_args.gpio->ival[0];
-            printf("GPIO: %d \n", pin);
+            printf("\nGPIO: %d \n", pin);
             ESP_LOGI("gpio-get", "GPIO # %i", gpio_get_args.gpio->ival[0]);
             //gpio_dump_io_configuration(stdout, 1ULL << gpio_get_args.gpio->ival[0]);
             printf("GPIO #%d level is %d\n", gpio_get_args.gpio->ival[0], gpio_get_level(gpio_get_args.gpio->ival[0]));
