7009a6d71e686f280d8cb7e52f47185592db6678
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-01-17 18:03:07 -0500

    ESP32-Console

--- ESP32-Console/main/cmd_gpio_set.h
+++ ESP32-Console/main/cmd_gpio_set.h
@@ -34,6 +34,7 @@ static int exec_gpio_set_cmd (int argc, char **argv) {
         */
         // Check that the minimal required arguments were passed, i.e. pin #, mode, and logic level.
         if (gpio_set_args.gpio->count > 0 && gpio_set_args.level->count > 0) {
+            // Display brief summary of passed arguments.
             ESP_LOGI("GPIO", "pin: %i, mode: %s, level: %i", gpio_set_args.gpio->ival[0], gpio_set_args.mode->sval[0], gpio_set_args.level->ival[0]);
             pin_config.pin_bit_mask = 1ULL << gpio_set_args.gpio->ival[0];
             pin_config.mode = GPIO_MODE_OUTPUT;
