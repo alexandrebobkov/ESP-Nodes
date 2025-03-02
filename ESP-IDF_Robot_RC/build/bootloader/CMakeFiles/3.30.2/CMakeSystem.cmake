2e02623ccab8cd9594883f3e7da3d59d92f6ec78
Alexander B <alexandre.bobkov@gmail.com>
2025-03-02 00:16:36 -0500

    esp32c3

--- ESP-IDF_Robot_RC/build/bootloader/CMakeFiles/3.30.2/CMakeSystem.cmake
+++ ESP-IDF_Robot_RC/build/bootloader/CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.11.0-13-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.11.0-18-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.11.0-13-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.11.0-18-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/alex/esp/v5.3.2/esp-idf/tools/cmake/toolchain-esp32c3.cmake")
