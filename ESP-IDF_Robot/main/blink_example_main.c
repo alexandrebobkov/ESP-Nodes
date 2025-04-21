6ef196edff71679280c507c5c06e3a62a37aae5b
Alexandre Bobkov <alexandre.bobkov@gmail.com>
2025-04-20 23:12:16 -0400

    ESP-IDF Robot

---
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -0,0 +1,389 @@
+/* Robot Controls
+    Generate PWM signals to control motors.
+
+    By: Alexander Bobkov
+    Date: Dec 21, 2024
+
+    built-in LED GPIO: 10
+    build-in push button GPIO: 3
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "driver/temperature_sensor.h"
+//#include "driver/mcpwm.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+/* ADC */
+#include "rc.h"
+/* Motors Controls */
+#include "motor_controls.h"
+/* System-wide controls */
+#include "controls.h"
+#include "esp_adc/adc_continuous.h"
+
+/* ESP-NOW */
+#include <string.h>
+#include <assert.h>
+#include "freertos/semphr.h"
+#include "freertos/timers.h"
+#include "nvs_flash.h"
+#include "esp_random.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_crc.h"
+#include "esp_netif.h"
+#include "esp_now.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "espnow_config.h"
+
+#include "config.h"
+
+static const char *TAG = "ESP IDF Robot";
+
+
+/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+
+// Retrieve values from configuration menu
+#define BLINK_GPIO                  CONFIG_BLINK_GPIO       // 10 GPIO of on-board LED
+#define PUSH_BTN_GPIO               CONFIG_BUTTON_GPIO      // 3 GPIO of on-board push-button
+#define MTR_FL_GPIO                 0 //CONFIG_MOTOR_FRONT_LEFT_GPIO
+// ADC
+#define ADC_ATTEN                   ADC_ATTEN_DB_11
+#define ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
+#define ADC_UNIT                    ADC_UNIT_1
+#define ADC_CONV_MODE               ADC_CONV_BOTH_UNIT// ADC_CONV_SINGLE_UNIT_1
+#define ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2    // ESP32C3
+#define READ_LEN                    1024//256
+#define ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
+#define ADC_GET_DATA(p_data)        ((p_data)->type2.data)
+#define PROJ_X                      (1)                     // ADC1_CH1; 0 GPIO joystick, x-axis
+#define PROJ_Y                      (0)                     // ADC1_CH0; 1 GPIO joystick, y-axis
+#define NAV_BTN                     (8)                     // 8 GPIO joystick button
+#define _ADC_UNIT_STR(unit)         #unit
+#define ADC_UNIT_STR(unit)          _ADC_UNIT_STR(unit)
+uint32_t x_avg = 0, y_avg = 0;
+static TaskHandle_t led_task_handle;
+static TaskHandle_t s_task_handle;
+static TaskHandle_t m_task_handle;  // Task for controlling motors PWMs
+static adc_channel_t channel[2] = {ADC_CHANNEL_0, ADC_CHANNEL_1};
+static sensors_data_t buf;
+
+#define ESP_INTR_FLAG_DEFAULT 0
+
+#define GPIO_INPUT_PIN_SEL ((1ULL<<PUSH_BTN_GPIO) | (1ULL<<NAV_BTN))
+#define GPIO_OUTPUT_PIN_SEL ((1ULL<<BLINK_GPIO))
+
+/* ESPNOW can work in both station and softap mode. It is configured in menuconfig. */
+#if CONFIG_ESPNOW_WIFI_MODE_STATION
+#define ESPNOW_WIFI_MODE WIFI_MODE_STA
+#define ESPNOW_WIFI_IF   ESP_IF_WIFI_STA
+#else
+#define ESPNOW_WIFI_MODE WIFI_MODE_AP
+#define ESPNOW_WIFI_IF   ESP_IF_WIFI_AP
+#endif
+
+static temperature_sensor_handle_t temp_sensor;
+static temperature_sensor_config_t temp_sensor_config;
+static float tsens_value;
+static QueueHandle_t gpio_evt_queue = NULL;
+static uint8_t s_led_state = 1;
+
+
+/*  ============================
+            ESP NOW
+    ============================
+
+    ESP32-C3 Blue board MAC:    54:32:04:46:71:80
+    ESP32-C3 SuperMini MAC:     34:b7:da:f9:33:8d
+    ESP32-C3 Breadboard MAC:    e4:b0:63:17:9e:45
+*/
+static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+//static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN]  = {0x54, 0x32, 0x04, 0x46, 0x71, 0x80};     // MAC address of troubleshooting Dev board
+//static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN]  = {0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45};
+static uint8_t robot_mac[ESP_NOW_ETH_ALEN]      = {0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45};     // MAC address of Robot
+static uint8_t rc_mac[ESP_NOW_ETH_ALEN]         = {0x34, 0xB7, 0xDA, 0xF9, 0x33, 0x8D};     // MAC address of Remote Control
+static uint8_t espnow_seq[ESPNOW_DATA_MAX]      = {0, 0};
+
+static int rc_x = 0, rc_y = 0;
+
+//uint8_t broadcastAddress[] = {};
+//struct_message controlData;
+esp_now_peer_info_t peerInfo;
+static void espnow_deinit(espnow_send_param_t *send_param);
+
+static void blink_led(void) {
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void IRAM_ATTR gpio_isr_handler (void* arg) {
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+// Push button interrupt task
+static void gpio_task (void* arg) {
+    uint32_t io_num;
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+static void nav_key_task (void* arg) {
+    uint32_t io_num;
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+
+static void configure_button (void) {
+    ESP_LOGI(TAG, "Configured on-board push button");
+    //gpio_reset_pin(PUSH_BTN_GPIO);
+    //gpio_set_direction(PUSH_BTN_GPIO, GPIO_MODE_INPUT);
+}
+
+static void motors_init (void) {
+    m.motor1_rpm_pcm = 0;
+    m.motor2_rpm_pcm = 0;
+    m.motor3_rpm_pcm = 0;
+    m.motor4_rpm_pcm = 0;
+}
+
+static void ledc_init (void) {
+
+    // MOTOR FRONT RIGHT, FORWARD
+    ledc_timer_config_t ledc_timer_1 = {
+        .speed_mode =       MTR_MODE,// LEDC_MODE,
+        .duty_resolution =  MTR_DUTY_RES,// LEDC_DUTY_RES,
+        .timer_num =        MTR_FRONT_RIGHT_TMR,// LEDC_TIMER,
+        .freq_hz =          MTR_FREQUENCY,// LEDC_FREQUENCY,
+        .clk_cfg =          LEDC_APB_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer_1));
+    ledc_channel_config_t ledc_channel_1 = {
+        .speed_mode =       MTR_MODE,
+        .channel =          MTR_FRONT_RIGHT,// LEDC_CHANNEL_0,// MTR_FRONT_RIGHT,
+        .timer_sel =        MTR_FRONT_RIGHT_TMR,// LEDC_TIMER,
+        .intr_type =        LEDC_INTR_DISABLE,
+        .gpio_num =         MTR_FRONT_RIGHT_IO,
+        .duty =             MTR_FRONT_RIGHT_DUTY,
+        .hpoint =           0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel_1));
+    // MOTOR FRONT LEFT, FORWARD
+    ledc_timer_config_t ledc_timer_2 = {
+        .speed_mode =       MTR_MODE,
+        .duty_resolution =  MTR_DUTY_RES,
+        .timer_num =        MTR_FRONT_LEFT_TMR,
+        .freq_hz =          MTR_FREQUENCY,
+        .clk_cfg =          LEDC_APB_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer_2));
+    ledc_channel_config_t ledc_channel_2 = {
+        .speed_mode =       MTR_MODE,
+        .channel =          MTR_FRONT_LEFT,
+        .timer_sel =        MTR_FRONT_LEFT_TMR,
+        .intr_type =        LEDC_INTR_DISABLE,
+        .gpio_num =         MTR_FRONT_LEFT_IO,
+        .duty =             MTR_FRONT_LEFT_DUTY,
+        .hpoint =           0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel_2));
+
+    // MOTOR FRONT RIGHT, REVERSE
+    ledc_timer_config_t ledc_timer_3 = {
+        .speed_mode =       MTR_MODE,// LEDC_MODE,
+        .duty_resolution =  MTR_DUTY_RES,// LEDC_DUTY_RES,
+        .timer_num =        MTR_FRONT_RIGHT_REV_TMR,// LEDC_TIMER,
+        .freq_hz =          MTR_FREQUENCY,// LEDC_FREQUENCY,
+        .clk_cfg =          LEDC_APB_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer_3));
+    ledc_channel_config_t ledc_channel_3 = {
+        .speed_mode =       MTR_MODE,
+        .channel =          MTR_FRONT_RIGHT_REV,// LEDC_CHANNEL_0,// MTR_FRONT_RIGHT,
+        .timer_sel =        MTR_FRONT_RIGHT_REV_TMR,// LEDC_TIMER,
+        .intr_type =        LEDC_INTR_DISABLE,
+        .gpio_num =         MTR_FRONT_RIGHT_REV_IO,
+        .duty =             MTR_FRONT_RIGHT_REV_DUTY,
+        .hpoint =           0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel_3));
+    // MOTOR FRONT LEFT, REVERSE
+    ledc_timer_config_t ledc_timer_4 = {
+        .speed_mode =       MTR_MODE,
+        .duty_resolution =  MTR_DUTY_RES,
+        .timer_num =        MTR_FRONT_LEFT_REV_TMR,
+        .freq_hz =          MTR_FREQUENCY,
+        .clk_cfg =          LEDC_APB_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer_4));
+    ledc_channel_config_t ledc_channel_4 = {
+        .speed_mode =       MTR_MODE,
+        .channel =          MTR_FRONT_LEFT_REV,
+        .timer_sel =        MTR_FRONT_LEFT_REV_TMR,
+        .intr_type =        LEDC_INTR_DISABLE,
+        .gpio_num =         MTR_FRONT_LEFT_REV_IO,
+        .duty =             MTR_FRONT_LEFT_REV_DUTY,
+        .hpoint =           0,
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel_4));
+}
+
+/* ESP-NOW */
+// Wi-Fi should start before using ESP-NOW
+static void wifi_init()
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
+    //ESP_ERROR_CHECK( esp_wifi_set_mode(CONFIG_ESPNOW_WIFI_MODE_STATION_SOFTAP) );
+    ESP_ERROR_CHECK( esp_wifi_start());
+    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
+}
+
+static void led_task (void *arg) {
+    while(1) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+	    gpio_set_level(BLINK_GPIO, s_led_state);
+	    vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        s_led_state = !s_led_state;
+	}
+}
+static void temp_sensor_task (void *arg) {
+    while (true) {
+        ESP_LOGI("ESP32-C3", "Reading sensor temperature");
+        float tsens_value;
+        ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_value));
+        ESP_LOGW("ESP32-C3", "Temperature value %.02f ℃", tsens_value);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
+// Task function to read joystick values and update motors rotation speeds.
+static void rc_task (void *arg) {
+    while (true) {
+        update_pwm (rc_x, rc_y);
+        ESP_LOGI("x,y", "( %d, %d ) [ %d, %d] ", rc_x, rc_y, x, y);
+        //vTaskDelay (10 / portTICK_PERIOD_MS);  // Determines responsiveness  
+        vTaskDelay (1000 / portTICK_PERIOD_MS); 
+    }
+}
+
+/*
+    EXP32-C3 Chip built-in temprature sensor
+*/
+static void chip_sensor_init () {
+    temp_sensor = NULL;
+    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(10, 50);
+    ESP_ERROR_CHECK(temperature_sensor_install(&temp_sensor_config, &temp_sensor));
+
+    ESP_LOGI(TAG, "Enable temperature sensor");
+    ESP_ERROR_CHECK(temperature_sensor_enable(temp_sensor));
+}
+static void display_chip_temperature () {
+    ESP_LOGI("ESP32-C3", "Reading sensor temperature");
+    ESP_ERROR_CHECK(temperature_sensor_get_celsius(temp_sensor, &tsens_value));
+    ESP_LOGW("ESP32-C3", "Temperature value %.02f ℃", tsens_value);
+}
+
+void onDataReceived (const uint8_t *mac_addr, const uint8_t *data, uint8_t data_len) {
+
+    memcpy(&buf, data, sizeof(buf));
+    rc_x = buf.x_axis;
+    rc_y = buf.y_axis;
+    update_pwm(rc_x, rc_y);    
+}
+
+void app_main(void)
+{
+    // Initialize internal temperature sensor
+    chip_sensor_init();
+    xTaskCreate(temp_sensor_task, "ESP32C3 Sensor", 2048, NULL, 15, NULL);
+
+    // Initialize LED
+    // Used to control the DC motor
+    ledc_init();
+    int var = 8191;
+    // Initialize the config structure.
+    gpio_config_t io_conf = {};
+
+    /* 
+        Configure on-board LED
+    */
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    xTaskCreate(led_task, "LED", 2048, NULL, 15, NULL);
+
+    // Configure on-board push button
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+    // Set push button interrupt
+    gpio_set_intr_type(PUSH_BTN_GPIO, GPIO_INTR_NEGEDGE);//ANYEDGE);
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(gpio_task, "GPIO task", 2048, NULL, 10, NULL);
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(PUSH_BTN_GPIO, gpio_isr_handler, (void*) PUSH_BTN_GPIO);
+
+    // Configure navigation button
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+    // Set navigation button interrupt
+    gpio_set_intr_type(NAV_BTN, GPIO_INTR_NEGEDGE);//ANYEDGE);
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(nav_key_task, "NAV Keys task", 2048, NULL, 10, NULL);
+    //gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(NAV_BTN, gpio_isr_handler, (void*) NAV_BTN);
+
+    configure_button();
+    //configure_dc_mc();
+    printf("Added button interrupt");
+
+    // ESP-NOW
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    buf.x_axis = 0;
+    buf.y_axis = 0;
+    buf.motor1_rpm_pcm = 0;
+    wifi_init();
+    esp_now_init();
+    esp_now_register_recv_cb(onDataReceived);
+
+    /*
+        ADC
+    */
+    rc_adc_init();
+    xTaskCreate(rc_task, "RC", 2048, NULL, 5, NULL);
+
+    /*
+        MOTORS
+    */
+    motors_init();
+}
---
+++ ESP-IDF_Robot/main/motor_controls.h
@@ -0,0 +1,27 @@
+#ifndef MOTOR_CONTROLS_H
+#define MOTOR_CONTROLSC_H
+
+// Interpolate value (x) based on raw reading, min/max limits.
+/*
+
+    Joystick scale:     4096    2048        0
+    PWM scale:          8191    4096        0
+
+    PWM Output:         +8191       0   -8191
+*/
+static int interpolate_raw_val (int raw) {
+    int x;
+
+    x = raw*2;
+
+    return x;
+}
+// Function that converts raw value from joystick scale (0 to 4096) to PCM scale (-8192 to 8192).
+static int rescale_raw_val (int raw) {
+
+    int s;
+    s = 4*raw - 8940;
+    return s;
+}
+
+#endif
