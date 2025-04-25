e0c883be4b7ecf6ef78be645b7d3a67b9b5caec4
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-04-25 03:14:53 -0400

    ESP32-C3 ePaper

--- ESP32-C3_ePaper/src/main.cpp
+++ ESP32-C3_ePaper/src/main.cpp
@@ -73,8 +73,8 @@ void setup() {
 // Display text nicely centered on the display
   display.setTextColor(GxEPD_WHITE);
   display.setFont(&FreeSansBold12pt7b);                                          // Specify the font family & size
-  display.getTextBounds("SERVER INFOBOARD", 10, 100, &tbx, &tby, &tbw, &tbh);   // Request width of text to be displayed
-  display.setCursor((display.width() - tbw) / 2, 100);                        // Calculate x-coordinate value
+  display.getTextBounds("SERVER INFOBOARD", 0, 0, &tbx, &tby, &tbw, &tbh);   // Request width of text to be displayed
+  display.setCursor((display.width() - tbw) / 2, 0);                        // Calculate x-coordinate value
   display.print("SERVER INFOBOARD");                                            // Print the text
   //display.update();
 
