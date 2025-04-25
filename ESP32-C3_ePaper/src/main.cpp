c589aeb2d31d96003fe8cb25379f7f8cbc364b72
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-04-25 03:31:35 -0400

    ESP32-C3 ePaper

--- ESP32-C3_ePaper/src/main.cpp
+++ ESP32-C3_ePaper/src/main.cpp
@@ -74,8 +74,11 @@ void setup() {
   display.setCursor(str_x, str_y+str_inc);
   display.setFont(&FreeMonoBold9pt7b);
   display.print("Backups");
+  // Files bar
   display.drawRect(str_x+100, str_y-str_inc, 100, str_inc, GxEPD_BLACK);
-  display.drawRect(str_x+100, str_y+str_inc*1, 100, str_inc, GxEPD_BLACK);
+  display.fillRect(str_x+100, str_y-str_inc, 100*0.25, str_inc, GxEPD_BLACK);
+  // Backups bar
+  display.drawRect(str_x+100, str_y+str_inc, 100, str_inc, GxEPD_BLACK);
 
 // Display text nicely centered on the display
   display.setTextColor(GxEPD_WHITE);
