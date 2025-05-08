3b10ffe589bfc63646ebddc1d87fb4312649df0e
Alexander B <alexandre.bobkov@gmail.com>
2025-05-08 15:05:08 -0400

    .

--- ESP-IDF_mqtt/main/app_main.c
+++ ESP-IDF_mqtt/main/app_main.c
@@ -29,7 +29,7 @@ static esp_mqtt5_user_property_item_t user_property_arr[] = {
         {"board", "esp32"},
         {"u", "user"},
         {"p", "password"}
-    };
+};
 
 #define USE_PROPERTY_ARR_SIZE   sizeof(user_property_arr)/sizeof(esp_mqtt5_user_property_item_t)
 
