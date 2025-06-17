7129cd9df3ec80f2d536ee5260faed93fbdeb18f
Alexander B <alexandre.bobkov@gmail.com>
2025-06-17 13:05:41 -0400

    ESP-NOW-Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
@@ -124,13 +124,7 @@ void app_main(void)
     //example_wifi_init();
     //example_espnow_init();
 
-    esp_err_t wifi_ret = wifi_init();
-    if (wifi_ret != ESP_OK) {
-        ESP_LOGE(TAG, "Error initializing ESPNOW");
-        ESP_LOGE(TAG, "wifi_init() failed: %s", esp_err_to_name(wifi_ret));
-        return;
-    }
-    ESP_LOGI(TAG, "Wi-Fi initialized successfully");
+    wifi_init();
 
     esp_err_t espnow_ret = esp_now_init();
     if (espnow_ret != ESP_OK) {
