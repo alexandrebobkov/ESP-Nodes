81b45b1d15beaf685b24bfee76188ddf97a45208
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-17 01:16:51 -0400

    Code comments

--- ESP-IDF_Robot_RC/main/espnow_example_main.c
+++ ESP-IDF_Robot_RC/main/espnow_example_main.c
@@ -32,7 +32,7 @@ typedef struct {
     int         x_axis;             // Joystick x-position
     int         y_axis;             // Joystick y-position
     bool        nav_bttn;           // Joystick push button
-    uint8_t     motor1_rpm_pcm;     // PCMs for 4 motors
+    uint8_t     motor1_rpm_pcm;     // PWMs for 4 motors
     uint8_t     motor2_rpm_pcm;
     uint8_t     motor3_rpm_pcm;
     uint8_t     motor4_rpm_pcm;
