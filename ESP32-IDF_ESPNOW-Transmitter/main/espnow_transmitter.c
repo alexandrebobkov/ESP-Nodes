36d8be77c260c01853ad99a5b187962a521d04b7
Alexander B <alexandre.bobkov@gmail.com>
2025-06-17 12:51:40 -0400

    ESP-NOW-Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
@@ -110,6 +110,8 @@ static void rc_send_data_task()
 
 void app_main(void)
 {
+    joystick_adc_init();
+    
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
