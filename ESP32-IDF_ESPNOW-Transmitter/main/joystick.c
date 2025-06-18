7793f00cc6b2876c9a0b79c9b2912cb23462449e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 01:28:24 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -37,3 +37,39 @@ void get_joystick_xy(int *x_axis, int *y_axis) {
     ESP_ERROR_CHECK(adc_oneshot_read(adc_xy_handle, ADC_CHANNEL_0, x_axis));
     ESP_ERROR_CHECK(adc_oneshot_read(adc_xy_handle, ADC_CHANNEL_1, y_axis));
 }
+
+void sendData (void)
+{
+    buffer.crc = 0;
+    buffer.x_axis = 240;
+    buffer.y_axis = 256;
+    buffer.nav_bttn = 0;
+    buffer.motor1_rpm_pcm = 0; //10;
+    buffer.motor2_rpm_pcm = 0;
+    buffer.motor3_rpm_pcm = 0;
+    buffer.motor4_rpm_pcm = 0;
+
+    get_joystick_xy(&x, &y);
+    //ESP_LOGI("(x, y)", "[ %d, %d ]", x, y);
+    buffer.x_axis = x;
+    buffer.y_axis = y;
+
+    // Display brief summary of data being sent.
+    ESP_LOGI(TAG, "Joystick (x,y) position ( %d, %d )", buffer.x_axis, buffer.y_axis);
+    ESP_LOGI(TAG, "pcm 1, pcm 2 [ 0x%04X, 0x%04X ]", (uint8_t)buffer.motor1_rpm_pcm, (uint8_t)buffer.motor2_rpm_pcm);
+    ESP_LOGI(TAG, "pcm 3, pcm 4 [ 0x%04X, 0x%04X ]", (uint8_t)buffer.motor3_rpm_pcm, (uint8_t)buffer.motor4_rpm_pcm);
+
+    // Call ESP-NOW function to send data (MAC address of receiver, pointer to the memory holding data & data length)
+    uint8_t result = esp_now_send(receiver_mac, (uint8_t *)&buffer, sizeof(buffer));
+
+    // If status is NOT OK, display error message and error code (in hexadecimal).
+    if (result != 0) {
+        ESP_LOGE("sendData()", "Error sending data! Error code: 0x%04X", result);
+        ESP_LOGE("sendData()", "esp_now_send() failed: %s", esp_err_to_name(result));
+        ESP_LOGE("sendData()", "Ensure that receiver is powered-on.");
+        ESP_LOGE("sendData()", "Ensure that received MAC is: %02X:%02X:%02X:%02X:%02X:%02X",
+                 receiver_mac[0], receiver_mac[1], receiver_mac[2],
+                 receiver_mac[3], receiver_mac[4], receiver_mac[5]);
+        deletePeer();
+    }
+}
