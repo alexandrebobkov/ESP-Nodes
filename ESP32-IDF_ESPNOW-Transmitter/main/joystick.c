58a82e0a6614892c73266c6eb10a4af9a8fe18f1
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-17 21:52:17 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -4,3 +4,15 @@ int convert_axis_to_pwm(int axis_value) {
     // and we want to map it to a PWM range of 0-255
     return (axis_value * 255) / 4095;
 }
+
+void sendRawData(void) {
+    
+    buffer.crc = 0;
+    buffer.x_axis = 240;
+    buffer.y_axis = 256;
+    buffer.nav_bttn = 0;
+    buffer.motor1_rpm_pcm = 0; //10;
+    buffer.motor2_rpm_pcm = 0;
+    buffer.motor3_rpm_pcm = 0;
+    buffer.motor4_rpm_pcm = 0;
+}
