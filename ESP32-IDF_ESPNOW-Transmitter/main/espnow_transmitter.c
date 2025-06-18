b7cc31e56513963aeaff74cb6abd03ca4b9cc5ec
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 00:07:03 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
@@ -58,7 +58,7 @@ void rc_send_data_task()
 {
     while (true) {
         if (esp_now_is_peer_exist(receiver_mac)) {
-            sendData();
+            //sendData();
             sendRawData(); }
         vTaskDelay (1000 / portTICK_PERIOD_MS);
     }
