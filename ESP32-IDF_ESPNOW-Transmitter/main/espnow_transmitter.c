ae9b047eae12803034aab5eb850e36c5f54fad04
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 00:46:21 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
@@ -21,7 +21,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include "freertos/timers.h"
-#include "driver/adc_oneshot.h"
+#include "esp_adc/adc_oneshot.h"
 #include "nvs_flash.h"
 #include "esp_random.h"
 #include "esp_event.h"
