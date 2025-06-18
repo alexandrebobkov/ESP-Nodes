6e54a9ff39bf1065d94fd4bef755c21a04d7833c
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 02:36:06 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
@@ -5,9 +5,9 @@
 #include "esp_now.h"
 #include "esp_netif.h"
 
-extern uint8_t broadcast_mac[ESP_NOW_ETH_ALEN];//      = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};     // Broadcast MAC address
-extern uint8_t receiver_mac[ESP_NOW_ETH_ALEN];//       = {0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45};     // MAC address of Robot
-extern uint8_t transmitter_mac[ESP_NOW_ETH_ALEN];//    = {0x34, 0xB7, 0xDA, 0xF9, 0x33, 0x8D};     // MAC address of Remote Control
+extern uint8_t broadcast_mac[ESP_NOW_ETH_ALEN];     // Broadcast MAC address
+extern uint8_t receiver_mac[ESP_NOW_ETH_ALEN];      // MAC address of Robot
+extern uint8_t transmitter_mac[ESP_NOW_ETH_ALEN];   // MAC address of Remote Control
 
 esp_err_t joystick_adc_init(void);
 void joystick_show_raw_xy();
