254f3ce116da6c76c0fbe24d07fe8f937ef26ab8
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 02:13:03 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -140,7 +140,7 @@ void wifi_init() {
 void rc_send_data_task()
 {
     while (true) {
-        if (esp_now_is_peer_exist(receiver_mac)) {
+        if (esp_now_is_peer_exist((uint8_t*)receiver_mac)) {
             sendData();
             //sendRawData(); 
         }
