a3cd7401a83c25147134c0702c81f22007e5e7c8
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 00:19:39 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -11,9 +11,9 @@
 
 #include "sensors_data.h"
 
-//static const char *TAG = "ESP-NOW_Transmitter"; 
+static const char *TAG = "ESP-NOW_Transmitter"; 
 
-//extern sensors_data_t buffer;
+extern sensors_data_t buffer;
 static int x, y; // Joystick x and y positions
 adc_oneshot_unit_handle_t adc_xy_handle;
 //uint8_t receiver_mac[ESP_NOW_ETH_ALEN]       = {0xE4, 0xB0, 0x63, 0x17, 0x9E, 0x45};     // MAC address of Robot
