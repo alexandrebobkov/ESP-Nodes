528434c2b29666269def5c03e29505defedbffdb
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-17 23:59:40 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/espnow_transmitter.c
@@ -46,7 +46,7 @@ void wifi_init() {
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(CONFIG_ESPNOW_WIFI_MODE));// ESPNOW_WIFI_MODE));
+    ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE));
     ESP_ERROR_CHECK( esp_wifi_start());
     ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
