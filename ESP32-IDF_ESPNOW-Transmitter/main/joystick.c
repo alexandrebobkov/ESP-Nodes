c377768df1af839f0009ae9bf96d51fd7c6a3657
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-17 22:13:47 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -1,6 +1,8 @@
 #include "sensors_data.h"
 #include "driver/adc.h"
 #include "esp_adc/adc_oneshot.h"
+#include "esp_err.h"
+#include "esp_log.h"
 
 static sensors_data_t buffer;
 static int x, y; // Joystick x and y positions
