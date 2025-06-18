fc62534d4b594987b920b67469561320a07726b9
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-17 21:52:45 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -1,3 +1,5 @@
+sensors_data_t buffer;
+
 int convert_axis_to_pwm(int axis_value) {
     // Convert the joystick axis value to a PWM value
     // Assuming axis_value is in the range of 0-4095 for a 12-bit ADC
