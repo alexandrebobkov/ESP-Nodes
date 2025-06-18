150a330c6cded211a9151b174536ca1194dc8d1b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-18 00:09:05 -0400

    ESP-NOW Transmitter

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
@@ -5,7 +5,7 @@
 #include "freertos/semphr.h"
 #include "freertos/timers.h"
 
-#include "driver/adc.h"
+//#include "driver/adc.h"
 #include "esp_adc/adc_oneshot.h"
 #include "esp_err.h"
 #include "esp_log.h"
