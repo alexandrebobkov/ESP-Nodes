a2c5f32383aeefb1e2073dde968eb69121ee6b4f
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 03:27:12 -0400

    temp sensor

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
@@ -12,12 +12,14 @@ void transmission_init();
 
 static void rc_send_data_task();
 static void sendData (void);
+static void get_joystick_xy(int *x_axis, int *y_axis);
+static void deletePeer (void);
 
 esp_err_t joystick_adc_init(void);
 void joystick_show_raw_xy();
-void get_joystick_xy(int *x_axis, int *y_axis);
 
-static void deletePeer (void);
+
+
 void joystick_task(void *arg);
 static void statusDataSend(const uint8_t *mac_addr, esp_now_send_status_t status);
 
