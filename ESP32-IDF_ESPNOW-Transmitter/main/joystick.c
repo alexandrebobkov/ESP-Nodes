59c96a067b5a59176fb688da56b3f788360fe67b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 03:04:15 -0400

    temp sensor

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -14,7 +14,7 @@
 #include "sensors_data.h"
 #include "config.h"
 
-static const char *TAG = "Joystick"; 
+//static const char *TAG = "Joystick"; 
 //const char *TAG = "ESP-NOW_Transmitter"; 
 
 esp_now_peer_info_t devices;
