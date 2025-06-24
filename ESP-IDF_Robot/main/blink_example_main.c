56f6e4acf509fa7df1f14a27f0d7958ca43b19a3
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-23 23:01:11 -0400

    .

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -268,7 +268,7 @@ static void wifi_init()
 
 static void led_task (void *arg) {
     while(1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        //ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
 	    gpio_set_level(BLINK_GPIO, s_led_state);
 	    vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
         s_led_state = !s_led_state;
