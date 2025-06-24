505f83b2f425d68640c2d5a174770838059b0d5e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 03:12:47 -0400

    temp sensor

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
@@ -5,10 +5,14 @@
 #include "esp_now.h"
 #include "esp_netif.h"
 
+#include "config.h"
 
-extern uint8_t broadcast_mac[ESP_NOW_ETH_ALEN];     // Broadcast MAC address
-extern uint8_t receiver_mac[ESP_NOW_ETH_ALEN];      // MAC address of Robot
-extern uint8_t transmitter_mac[ESP_NOW_ETH_ALEN];   // MAC address of Remote Control
+
+//extern uint8_t broadcast_mac[ESP_NOW_ETH_ALEN];     // Broadcast MAC address
+//extern uint8_t receiver_mac[ESP_NOW_ETH_ALEN];      // MAC address of Robot
+//extern uint8_t transmitter_mac[ESP_NOW_ETH_ALEN];   // MAC address of Remote Control
+
+void transmission_init();
 
 esp_err_t joystick_adc_init(void);
 void joystick_show_raw_xy();
@@ -19,7 +23,7 @@ void joystick_task(void *arg);
 void statusDataSend(const uint8_t *mac_addr, esp_now_send_status_t status);
 void wifi_init();
 void rc_send_data_task();
-void transmission_init();
+
 
 
 #endif
