71e911db352a73f6dad2ce3d4d10f4c958c38f01
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 01:28:27 -0400

    temp sensor

--- ESP32-IDF_ESPNOW-Transmitter/main/system_health.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/system_health.h
@@ -1,6 +1,7 @@
 #ifndef ESPNOW_EXAMPLE_H
 #define ESPNOW_EXAMPLE_H
 
-static void temp_sensor_task (void *arg) ;
+static void chip_sensor_init ();
+static void temp_sensor_task (void *arg);
 
 #endif
