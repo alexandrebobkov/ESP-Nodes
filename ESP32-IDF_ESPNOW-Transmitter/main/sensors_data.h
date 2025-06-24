90618456c8843e0790911b3df1c1c148f6661f53
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 00:56:46 -0400

    .

--- ESP32-IDF_ESPNOW-Transmitter/main/sensors_data.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/sensors_data.h
@@ -10,6 +10,7 @@ typedef struct {
     int         x_axis;             // Joystick x-position
     int         y_axis;             // Joystick y-position
     bool        nav_bttn;           // Joystick push button
+    bool        led;                // LED ON/OFF state
     uint8_t     motor1_rpm_pwm;     // PWMs for 4 DC motors
     uint8_t     motor2_rpm_pwm;
     uint8_t     motor3_rpm_pwm;
