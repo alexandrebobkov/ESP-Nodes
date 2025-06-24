3cdbbada1ed426bd2df2eca1bdf809f3e01bb55b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 02:49:59 -0400

    temp sensor

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.h
@@ -4,18 +4,7 @@
 #include "esp_mac.h"
 #include "esp_now.h"
 #include "esp_netif.h"
-#include <stdlib.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "esp_adc/adc_oneshot.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "esp_wifi.h"
-#include "esp_log.h"
-#include "esp_err.h"
-#include "esp_mac.h"
-#include "esp_now.h"
+
 
 extern uint8_t broadcast_mac[ESP_NOW_ETH_ALEN];     // Broadcast MAC address
 extern uint8_t receiver_mac[ESP_NOW_ETH_ALEN];      // MAC address of Robot
