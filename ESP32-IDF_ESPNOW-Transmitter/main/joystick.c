dc2c6fb2b1d471597af67556f10fb38b44fd0c2c
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-06-24 04:28:37 -0400

    temp sensor

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -117,7 +117,7 @@ static void statusDataSend(const uint8_t *mac_addr, esp_now_send_status_t status
         deletePeer();
         //esp_restart();
         esp_now_deinit();
-        esp_now_init();
+        transmission_init();
         ESP_LOGI(TAG, "Re-initializing ESPNOW...");
     }
 }
