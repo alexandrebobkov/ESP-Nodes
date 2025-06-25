3fa0738314da5e978688aff2b1c17cc8c3450cd4
Alexander B <alexandre.bobkov@gmail.com>
2025-06-25 02:44:43 -0400

    adc

---
+++ ESP-IDF_Robot/build/managed_components_list.temp.cmake
@@ -0,0 +1,3 @@
+idf_build_component("/home/alex/github/ESP-Nodes/ESP-IDF_Robot/managed_components/espressif__led_strip" "project_managed_components")
+idf_component_set_property(espressif__led_strip COMPONENT_VERSION "2.5.5")
+set(managed_components "espressif__led_strip")
