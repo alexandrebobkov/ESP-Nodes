f0bf257b0ad4dfa634ed0eca3d2372163c5f6a8d
Alexander B <alexandre.bobkov@gmail.com>
2025-06-25 02:43:49 -0400

    adc

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -147,7 +147,8 @@ static void update_pwm (int rc_x, int rc_y) {
         m.motor3_rpm_pcm = -x;
         m.motor4_rpm_pcm = -x;
     }
-        else if ((x > 0 && x < 500) && (y < 200)) {
+    
+    else if ((x > -200 && x < 500) && (y < 200)) {
         //else if ((y < 0 && y > -200) && (x < -1000)) {
             //ESP_LOGW("ESP-NOW", "LEFT");
             // Left side rotates in forward direction, right side rotates in reverse direction.
@@ -155,16 +156,16 @@ static void update_pwm (int rc_x, int rc_y) {
             m.motor2_rpm_pcm = 0;
             m.motor3_rpm_pcm = -y;
             m.motor4_rpm_pcm = 0;
-        }
-        else if ((x > 0 && x < 500) && (y > 600)) {
+    }
+    else if ((x > -200 && x < 500) && (y > 600)) {
         //else if ((y < 0 && y > -200) && (x > 1000)) {
             //ESP_LOGW("ESP-NOW", "RIGHT");
             // Right side rotates in forward direction, left side rotates in reverse direction.
-            m.motor1_rpm_pcm = 0;
-            m.motor2_rpm_pcm = y; 
-            m.motor3_rpm_pcm = 0;
-            m.motor4_rpm_pcm = y; 
-        }
+        m.motor1_rpm_pcm = 0;
+        m.motor2_rpm_pcm = y; 
+        m.motor3_rpm_pcm = 0;
+        m.motor4_rpm_pcm = y; 
+    }
         else {
             //ESP_LOGW("ESP-NOW", "STAND STILL");
             m.motor1_rpm_pcm = 0;
