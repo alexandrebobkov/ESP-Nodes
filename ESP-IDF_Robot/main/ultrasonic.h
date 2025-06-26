8da293b08e8896dc02d3722ddf595eb0a496f7c0
Alexander B <alexandre.bobkov@gmail.com>
2025-06-26 14:03:27 -0400

    Ultrasonic

--- ESP-IDF_Robot/main/ultrasonic.h
+++ ESP-IDF_Robot/main/ultrasonic.h
@@ -1,4 +1,9 @@
 #ifndef __ULTRASONIC_H__
 #define __ULTRASONIC_H__
 
+typedef struct {
+    gpio_num_t trigger_gpio;  // GPIO for the trigger pin
+    gpio_num_t echo_gpio;     // GPIO for the echo pin
+} ultrasonic_sensor_t;
+
 #endif
