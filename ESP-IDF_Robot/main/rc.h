34e4d0d7b4d0e6b31c5f0dcb12e1a08eb9f88895
Alexander B <alexandre.bobkov@gmail.com>
2025-06-26 03:25:44 -0400

    PWM rescaling

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -164,21 +164,8 @@ static void update_pwm (int rc_x, int rc_y) {
         m.motor3_rpm_pcm = 0;
         m.motor4_rpm_pcm = y; 
     }
-    // DIAGONALLY FORWARD
-    /*else if ((x > 7400) && (y > 7400)) {
-        m.motor1_rpm_pcm = 0;   // left, forward
-        m.motor2_rpm_pcm = 0;   // right, forward
-        m.motor3_rpm_pcm = 0;
-        m.motor4_rpm_pcm = x;
-    }
-    else if ((x > 7400) && (y < 0)) {
-        m.motor1_rpm_pcm = 0;
-        m.motor2_rpm_pcm = 0;
-        m.motor3_rpm_pcm = 0;
-        m.motor4_rpm_pcm = -x;
-    }*/
     else {
-            //ESP_LOGW("ESP-NOW", "STAND STILL");
+        //ESP_LOGW("ESP-NOW", "STAND STILL");
         m.motor1_rpm_pcm = 0;
         m.motor2_rpm_pcm = 0;
         m.motor3_rpm_pcm = 0;
