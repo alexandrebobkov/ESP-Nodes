e9d3ba950be747b6a66336ef9a84cd73f2655db4
Alexander B <alexandre.bobkov@gmail.com>
2025-06-26 14:05:36 -0400

    Ultrasonic

--- ESP-IDF_Robot/main/ultrasonic.h
+++ ESP-IDF_Robot/main/ultrasonic.h
@@ -10,5 +10,6 @@ typedef struct {
 } ultrasonic_sensor_t;
 
 esp_err_t ultrasonic_init (const ultrasonic_sensor_t *sensor);
+esp_err_t ultrasonic_measure_raw (const ultrasonic_sensor_t *sensor,uint32_t max_time_us, uint32_t *time_us);
 
 #endif
