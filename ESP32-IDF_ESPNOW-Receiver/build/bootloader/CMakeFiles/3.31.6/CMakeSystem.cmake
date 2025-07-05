593166a9dcd25e4be9eff25e575dab90c23c2085
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-07-05 00:03:04 -0400

    receiver

--- ESP32-IDF_ESPNOW-Receiver/build/bootloader/CMakeFiles/3.31.6/CMakeSystem.cmake
+++ ESP32-IDF_ESPNOW-Receiver/build/bootloader/CMakeFiles/3.31.6/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.14.0-22-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.14.0-23-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.14.0-22-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.14.0-23-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/abobkov/esp/v5.4.1/esp-idf/tools/cmake/toolchain-esp32c3.cmake")
