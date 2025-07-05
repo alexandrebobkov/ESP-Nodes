7c25d3fac8a7f3b723b30df64d0d58a34f431bb3
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-07-04 23:56:35 -0400

    receiver

---
+++ ESP32-IDF_ESPNOW-Receiver/main/espnow_receiver.c
@@ -0,0 +1,18 @@
+/*  ESPNOW Receiver
+    by: Alexander Bobkov
+    Date Created:   July 4, 2025
+    Updated:        July 4, 2025
+    SDK:            ESP-IDF v.5.4.1
+
+   
+    
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/*
+   This program uses ESPNOW for receiving joystick x- and y- axis values from the receiving device.
+*/
