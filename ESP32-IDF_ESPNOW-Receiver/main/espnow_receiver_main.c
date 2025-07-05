4997c45decd8b93fe7122b581589fba5f4402327
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-07-05 01:14:21 -0400

    receiver

--- ESP32-IDF_ESPNOW-Receiver/main/espnow_receiver_main.c
+++ ESP32-IDF_ESPNOW-Receiver/main/espnow_receiver_main.c
@@ -17,6 +17,7 @@
    This program uses ESPNOW for receiving joystick x- and y- axis values from the receiving device.
 */
 
+#include <tdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "nvs_flash.h"
 #include "esp_err.h"
