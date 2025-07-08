64f42ff3ca9186e56694ab933cb2452a1f1447f7
Alexander B <alexandre.bobkov@gmail.com>
2025-07-08 00:45:36 -0400

    INA219

--- ESP-IDF_Robot/build/bootloader/cmake_install.cmake
+++ ESP-IDF_Robot/build/bootloader/cmake_install.cmake
@@ -42,12 +42,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/bootloader/esp-idf/cmake_install.cmake")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/bootloader/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -61,6 +55,8 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
   file(WRITE "/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/bootloader/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- ESP-IDF_Robot/build/bootloader/esp-idf/cmake_install.cmake
+++ ESP-IDF_Robot/build/bootloader/esp-idf/cmake_install.cmake
@@ -142,9 +142,3 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/bootloader/esp-idf/main/cmake_install.cmake")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/bootloader/esp-idf/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
