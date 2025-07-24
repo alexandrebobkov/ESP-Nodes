b0d6ed39df9e1ea8ca81d4e2567444eb3d2ca418
Alexandre B <alexandre.bobkov@gmail.com>
2025-07-23 22:48:26 -0400

    .

--- ESP-IDF_Robot/main/mqtt.c
+++ ESP-IDF_Robot/main/mqtt.c
@@ -1,10 +1,11 @@
 #include "mqtt_client.h"
+#include "esp_wifi.h"
 #include "esp_log.h"
 
 #include "mqtt.h"
 
-static const char* WIFI_ID =            "IoT_bots";
-static const char* WIFI_PASSWORD =      "208208208";
+//static const char WIFI_SSID =            "IoT_bots";
+//static const char WIFI_PASSWORD =      "208208208";
 static const char* MQTT_BROKER_URI =    "mqtt://techquadbit.net";
 static const char* MQTT_TAG =           "MQTT_Robot";
 
@@ -24,7 +25,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         default:
             break;
     }
-
 }
 
 static void mqtt_app_start(void) {
@@ -35,4 +35,25 @@ static void mqtt_app_start(void) {
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
+}
+
+static void wifi_init(void) {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+    esp_wifi_connect();
 }
--- ESP-IDF_Robot/main/mqtt.h
+++ ESP-IDF_Robot/main/mqtt.h
@@ -4,12 +4,15 @@
 #include "mqtt_client.h"
 #include "esp_wifi.h"
 
-static const char* WIFI_SSID;
-static const char* WIFI_PASSWORD;
+//static const char WIFI_SSID;
+#define WIFI_SSID "IoT_bots"
+//static const char WIFI_PASSWORD;
+#define WIFI_PASSWORD "208208208"
 static const char* MQTT_BROKER_URI;
 static const char* MQTT_TAG;
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 static void mqtt_app_start(void);
+static void wifi_init(void);
 
 #endif
