fb0c62fb39f06193a5b58031241e2f5ebcba89ee
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 19:37:47 -0400

    RC Calibration

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -520,12 +520,12 @@ void app_main(void)
 
     // ESP-NOW
     // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
+    /*esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK( nvs_flash_erase() );
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );
+    ESP_ERROR_CHECK( ret );*/
 
     // Initialize buffer with 0s
     buf.x_axis = 0;
