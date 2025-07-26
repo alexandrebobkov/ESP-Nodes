dc340476ddc85501fc5e1c7d227a37b0e12e3113
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 19:17:44 -0400

    RC Calibration

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -460,7 +460,7 @@ void app_main(void)
     ESP_ERROR_CHECK(wifi_ret);
 
     //wifi_init_sta();
-    wifi_init();
+    //wifi_init();
     // Wait for Wi-Fi connection
     xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
     mqtt_app_start();
