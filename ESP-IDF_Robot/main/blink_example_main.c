370a93ec3eaf52a8d348a596e3048eb021903b82
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 19:54:57 -0400

    RC Calibration

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -462,12 +462,12 @@ static void wifi_init_sta(void) {
 
 void app_main(void)
 {
-    esp_err_t wifi_ret = nvs_flash_init();
+    /*esp_err_t wifi_ret = nvs_flash_init();
     if (wifi_ret == ESP_ERR_NVS_NO_FREE_PAGES || wifi_ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         wifi_ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK(wifi_ret);
+    ESP_ERROR_CHECK(wifi_ret);*/
 
     //wifi_init_sta();
     //wifi_init();
@@ -525,12 +525,12 @@ void app_main(void)
 
     // ESP-NOW
     // Initialize NVS
-    /*esp_err_t ret = nvs_flash_init();
+    esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK( nvs_flash_erase() );
         ret = nvs_flash_init();
     }
-    ESP_ERROR_CHECK( ret );*/
+    ESP_ERROR_CHECK( ret );
 
     // Initialize buffer with 0s
     buf.x_axis = 0;
