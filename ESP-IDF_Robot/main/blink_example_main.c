0f0682799d1e2e8ef9d8b633fc1e4064d4597682
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 20:53:52 -0400

    RC Calibration

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -303,6 +303,7 @@ static void wifi_init()
         .sta = {
             .ssid = "IoT_bots",
             .password = "208208208",
+            .channel = 11,
         },
     };
     ESP_ERROR_CHECK (esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
