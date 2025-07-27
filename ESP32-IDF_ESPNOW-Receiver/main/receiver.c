e586df642b97c787620e66b832f805fdd5b83637
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 20:57:46 -0400

    RC Calibration

--- ESP32-IDF_ESPNOW-Receiver/main/receiver.c
+++ ESP32-IDF_ESPNOW-Receiver/main/receiver.c
@@ -22,7 +22,7 @@ void wifi_init()
     /*
     * STAND-ALONE ESP-NOW
     */
-    /*ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
@@ -32,12 +32,12 @@ void wifi_init()
     ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
-    #endif*/
+    #endif
 
     /*
     * WI-FI
     */
-    ESP_ERROR_CHECK(esp_netif_init());
+    /*ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
@@ -52,7 +52,7 @@ void wifi_init()
     };
     ESP_ERROR_CHECK (esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK( esp_wifi_start());
-    ESP_ERROR_CHECK( esp_wifi_connect());
+    ESP_ERROR_CHECK( esp_wifi_connect());*/
 }
 
 void transmission_init() {
