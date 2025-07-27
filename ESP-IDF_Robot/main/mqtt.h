5fedd97707c3087cf113e73fa6833ba5ea0fe586
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 23:12:51 -0400

    ESP-NOW + Wi-Fi

--- ESP-IDF_Robot/main/mqtt.h
+++ ESP-IDF_Robot/main/mqtt.h
@@ -10,8 +10,10 @@
 #define WIFI_PASSWORD "208208208"
 static const char* MQTT_BROKER_URI;
 static const char* MQTT_TAG;
+static esp_mqtt_client_handle_t mqtt_client;;
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+static void mqtt_publish_task(void *arg);
 void mqtt_app_start(void);
 void mqtt_publish(void);
 void sta_wifi_init(void);
