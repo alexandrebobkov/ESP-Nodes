9bef9dfdcc37a208bf93eb3f0b404fbc0b5011c1
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 21:00:04 -0400

    RC Calibration

--- ESP32-IDF_ESPNOW-Receiver/main/receiver.c
+++ ESP32-IDF_ESPNOW-Receiver/main/receiver.c
@@ -28,7 +28,7 @@ void wifi_init()
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     //ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA));//ESPNOW_WIFI_MODE));
-    ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE));
+    ESP_ERROR_CHECK( esp_wifi_set_mode((ESPNOW_WIFI_MODE));
     ESP_ERROR_CHECK( esp_wifi_start());
     //ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     ESP_ERROR_CHECK( esp_wifi_set_channel(11, WIFI_SECOND_CHAN_NONE));
