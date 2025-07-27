60a2471f74a7e8956b40b7434098fbc3ee1b6c0a
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 20:31:40 -0400

    RC Calibration

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -93,12 +93,10 @@ static void update_pwm (int rc_x, int rc_y) {
     if (x == 8190 && y == -8190) {
         m.motor1_rpm_pcm = 4095;
         m.motor2_rpm_pcm = 8190;
-
     }
     else if (x == 8190 && y == 8190) {
         m.motor1_rpm_pcm = 8190;
         m.motor2_rpm_pcm = 4095;
-
     }
     // FORWARD AND REVERSE
     //if ((x > 1500) && (y > 700 && y < 850)) {
