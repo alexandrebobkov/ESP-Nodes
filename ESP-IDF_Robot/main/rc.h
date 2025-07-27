c53a24559c7d9e4f25c819934b158ccf37dca1bc
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 20:08:08 -0400

    RC Calibration

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -91,7 +91,7 @@ static void update_pwm (int rc_x, int rc_y) {
 
     // FORWARD AND REVERSE
     //if ((x > 1500) && (y > 700 && y < 850)) {
-    if ((x > 1500) && (y > 0 && y < 2500)) {
+    if ((x > 1500) && (y > -2500 && y < 2500)) {
         //ESP_LOGW("ESP-NOW", "FORWARD");
         // Both sides rotate in forward direction.
         m.motor1_rpm_pcm = x;   // left, forward
@@ -100,7 +100,7 @@ static void update_pwm (int rc_x, int rc_y) {
         m.motor4_rpm_pcm = 0;
     }
     //else if ((x < 0) && (y > 700 && y < 850)) {
-    else if ((x < 0) && (y > 0 && y < 2500)) {
+    else if ((x < 0) && (y > -2500 && y < 2500)) {
         //ESP_LOGW("ESP-NOW", "REVERSE");
         // Both sides rotate in reverse direction.
         m.motor1_rpm_pcm = 0;
