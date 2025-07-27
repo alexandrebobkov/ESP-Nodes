3e7673fb9a2f7efcdfa25ac5f04f0c296a3bd6bc
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 20:47:58 -0400

    RC Calibration

--- ESP32-IDF_ESPNOW-Receiver/main/receiver.c
+++ ESP32-IDF_ESPNOW-Receiver/main/receiver.c
@@ -22,7 +22,7 @@ void wifi_init()
     /*
     * STAND-ALONE ESP-NOW
     */
-    ESP_ERROR_CHECK(esp_netif_init());
+    /*ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
@@ -32,7 +32,7 @@ void wifi_init()
     ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
-    #endif
+    #endif*/
 
     /*
     * WI-FI
