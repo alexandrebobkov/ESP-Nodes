c0f73ead409ab433e82c8c95fa34598167120971
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 20:46:28 -0400

    RC Calibration

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -278,9 +278,6 @@ static void wifi_init()
     /*
     * STAND-ALONE
     */
-    /*
-    * WI-FI
-    */
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -292,7 +289,7 @@ static void wifi_init()
     #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
     #endif
-    
+
     /*
     * WI-FI
     */
