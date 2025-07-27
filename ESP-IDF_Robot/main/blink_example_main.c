073b9ff1b420cedc4d061c4193ed32440900074b
Alexander B <alexandre.bobkov@gmail.com>
2025-07-26 23:00:21 -0400

    ESP-NOW + Wi-Fi

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -295,6 +295,7 @@ static void wifi_init()
     */
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_netif_create_default_wifi_sta());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
@@ -310,7 +311,7 @@ static void wifi_init()
     ESP_ERROR_CHECK( esp_wifi_start());
     //ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     
-    //ESP_ERROR_CHECK( esp_wifi_connect() );
+    ESP_ERROR_CHECK( esp_wifi_connect() );
 }
 
 static void led_task (void *arg) {
