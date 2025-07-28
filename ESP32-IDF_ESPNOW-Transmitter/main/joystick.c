371706704ad4c6af66545cccd13b8388efee5478
Alexander B <alexandre.bobkov@gmail.com>
2025-07-28 15:45:04 -0400

    ESPNOW + WiFi

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -138,7 +138,10 @@ static void statusDataSend(const uint8_t *mac_addr, esp_now_send_status_t status
 /* WiFi should start before using ESPNOW */
 void wifi_init()
 {
-    ESP_ERROR_CHECK(esp_netif_init());
+    /*
+    * STAND-ALONE
+    */
+    /*ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
@@ -149,7 +152,29 @@ void wifi_init()
     ESP_ERROR_CHECK( esp_wifi_set_channel(11, WIFI_SECOND_CHAN_NONE));
     #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
-    #endif
+    #endif*/
+
+    /*
+    * WI-FI
+    */
+   ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA));//ESPNOW_WIFI_MODE));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "IoT_bots",
+            .password = "208208208",
+        },
+    };
+    ESP_ERROR_CHECK (esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    //ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
+    ESP_ERROR_CHECK( esp_wifi_start());
+    //ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
+    ESP_ERROR_CHECK( esp_wifi_connect() );
 }
 
 // Task to periodically send ESPNOW remote control data
