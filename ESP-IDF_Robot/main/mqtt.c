bdd360f553b4f3b7e64d92e1a159e88eb4f71c79
Alexander B <alexandre.bobkov@gmail.com>
2025-07-28 16:57:58 -0400

    ESPNOW + WiFi

--- ESP-IDF_Robot/main/mqtt.c
+++ ESP-IDF_Robot/main/mqtt.c
@@ -30,6 +30,8 @@ static void mqtt_publish_task(void *arg) {
     }
 }
 
+void mqtt_publish_temp () {}
+
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
     
     esp_mqtt_event_handle_t event = event_data;
