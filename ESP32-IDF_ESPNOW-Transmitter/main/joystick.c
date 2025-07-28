c50b961fb4c91d767e3cda801d1999bb721e2e5d
Alexander B <alexandre.bobkov@gmail.com>
2025-07-28 15:57:30 -0400

    ESPNOW + WiFi

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -141,7 +141,7 @@ void wifi_init()
     /*
     * STAND-ALONE
     */
-    ESP_ERROR_CHECK(esp_netif_init());
+    /*ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
@@ -152,11 +152,11 @@ void wifi_init()
     ESP_ERROR_CHECK( esp_wifi_set_channel(11, WIFI_SECOND_CHAN_NONE));
     #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
-    #endif
+    #endif*/
 
     /*
     * WI-FI
-    */
+    
     /*ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
@@ -174,7 +174,7 @@ void wifi_init()
     //ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
     ESP_ERROR_CHECK( esp_wifi_start());
     //ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
-    ESP_ERROR_CHECK( esp_wifi_connect() );*/
+    ESP_ERROR_CHECK( esp_wifi_connect() );
 }
 
 // Task to periodically send ESPNOW remote control data
