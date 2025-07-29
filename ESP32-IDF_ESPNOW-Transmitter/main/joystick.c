a5427a113fc3fd770681483568868dcea0e67da6
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-07-29 01:43:08 -0400

    espnow autoreconnect

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -148,6 +148,9 @@ static void statusDataSend(const uint8_t *mac_addr, esp_now_send_status_t status
             espnow_channel = 1;
         }
 
+        devices.channel = espnow_channel;
+        esp_now_add_peer(&devices);
+
         //esp_now_deinit();  // Stop ESP-NOW
         //wifi_init();
         //esp_now_init();
