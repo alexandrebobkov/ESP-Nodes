2ebfa49ee614282b462d5dde62fe99567a60ef9d
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-07-29 04:54:55 -0400

    espnow autoreconnect

--- ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
+++ ESP32-IDF_ESPNOW-Transmitter/main/joystick.c
@@ -18,7 +18,7 @@ esp_now_peer_info_t devices;
 static adc_oneshot_unit_handle_t adc_xy_handle;
 sensors_data_t buffer;
 static int x, y; // Joystick x- and y- axis positions
-static int espnow_channel = 1;
+static int espnow_channel = 11;
 void transmission_init();
 void wifi_init();
 
@@ -156,7 +156,7 @@ static void statusDataSend(const uint8_t *mac_addr, esp_now_send_status_t status
         devices.channel = espnow_channel;
         devices.encrypt = false;
         esp_now_add_peer(&devices);
-        
+
         //esp_now_deinit();  // Stop ESP-NOW
         //wifi_init();
         //esp_now_init();
