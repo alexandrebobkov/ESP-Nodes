8f6f00c7f4143da39122e38173b44116e398e0b9
Alexandre B <alexandre.bobkov@gmail.com>
2025-08-03 01:43:02 -0400

    core

--- ESP32-IDF_ESPNOW-Transmitter/main/sensors_data.h
+++ ESP32-IDF_ESPNOW-Transmitter/main/sensors_data.h
@@ -6,12 +6,12 @@
 #include <stdbool.h>
 
 typedef struct {
-    uint16_t    crc;                // CRC16 value of ESPNOW data
-    int         x_axis;             // Joystick x-position
-    int         y_axis;             // Joystick y-position
-    bool        nav_bttn;           // Joystick push button
-    bool        led;                // LED ON/OFF state
-    uint8_t     motor1_rpm_pwm;     // PWMs for 4 DC motors
+    uint16_t    crc;                
+    int         sensor1;            
+    int         sensor2;             
+    bool        sensor3;          
+    bool        sensor4;              
+    uint8_t     motor1_rpm_pwm;     
     uint8_t     motor2_rpm_pwm;
     uint8_t     motor3_rpm_pwm;
     uint8_t     motor4_rpm_pwm;
