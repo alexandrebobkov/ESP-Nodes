5e6a93e287f665e75aba90264898e9646f6883ca
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-08-05 03:02:34 -0400

    FreeRTOS

--- ESP32-IDF_FreeRTOS/I2C_MultiDevice/main/hello_world_main.c
+++ ESP32-IDF_FreeRTOS/I2C_MultiDevice/main/hello_world_main.c
@@ -165,10 +165,13 @@ void display_task(void *pvParameters) {
     SensorsData data;
     while (1) {
         if (xQueueReceive(xQueue1, &data, 500)) {
-            printf("Display Task received from Task 1: num1=%" PRIu32 ", num2=%" PRIu32 ", num3=%" PRIu32 "\n", data.num1, data.num2, data.num3);
+            printf("Display Task received data from Task 1: num1=%" PRIu32 ", num2=%" PRIu32 ", num3=%" PRIu32 "\n", data.num1, data.num2, data.num3);
+        }
+        if (xQueueReceive(xQueue1, &s_data, 500)) {
+            printf("Display Task received data from Task 1: num1=%" PRIu32 ", num2=%" PRIu32 ", num3=%" PRIu32 "\n", s_data.num1, s_data.num2, s_data.num3);
         }
         if (xQueueReceive(xQueue2, &data, 500)) {
-            printf("Display Task received from Task 2: num1=%" PRIu32 ", num2=%" PRIu32 ", num3=%" PRIu32 "\n", data.num1, data.num2, data.num3);
+            printf("Display Task received data from Task 2: num1=%" PRIu32 ", num2=%" PRIu32 ", num3=%" PRIu32 "\n", data.num1, data.num2, data.num3);
         }
         vTaskDelay(500);
     }
