18aad6bf708a554dad1cac2f46992fe06513e85e
Alexandre Bobkov <alexandre.bobkov@gmail.com>
2025-08-05 08:53:48 -0400

    i2c io

--- ESP32-IDF_FreeRTOS/I2C_MultiDevice/main/i2c_io.h
+++ ESP32-IDF_FreeRTOS/I2C_MultiDevice/main/i2c_io.h
@@ -6,6 +6,9 @@
 #include "freertos/semphr.h"
 #include "driver/i2c_master.h"
 
+#define I2C_GPIO_SCL (22)
+#define I2C_GPIO_SDA (21)
+
 typedef struct {
     SemaphoreHandle_t i2cMutex;
     uint16_t sda;
@@ -15,8 +18,8 @@ typedef struct {
 i2c_master_bus_config_t bus_config = {
     .clk_source = I2C_CLK_SRC_DEFAULT,
     .i2c_port = I2C_NUM_0,               // Use I2C_NUM_1 for second port
-    .scl_io_num = 22,           // Your SCL pin
-    .sda_io_num = 21,           // Your SDA pin
+    .scl_io_num = I2C_GPIO_SCL,           // Your SCL pin
+    .sda_io_num = I2C_GPIO_SDA,           // Your SDA pin
     .glitch_ignore_cnt = 7,
     .flags.enable_internal_pullup = true,
 };
