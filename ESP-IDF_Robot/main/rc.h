c43111177ef2be16df5f7436fb57cfc0802324ef
Alexandre Bobkov <alexandre.bobkov@gmail.com>
2025-08-06 04:47:52 -0400

    pcm

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -118,7 +118,7 @@ static void update_pwm (int rc_x, int rc_y) {
         m.motor3_rpm_pcm = -(y - x);
         m.motor4_rpm_pcm = 0;
     }
-    // Rotate right
+    // Rotate left
     if ((y < 1500) && (y > -1500) && (x < -8000)) {
         m.motor1_rpm_pcm = 0;
         m.motor2_rpm_pcm = (y + x);
