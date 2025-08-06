84858114856bf5d1479fa46a5e7d2187c6ac936f
Alexander B <alexandre.bobkov@gmail.com>
2025-08-06 08:53:58 -0400

    pwm

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -104,7 +104,7 @@ static void update_pwm (int rc_x, int rc_y) {
         m.motor4_rpm_pcm = 0;
     }
     else if (x >= 1500 && y >= -8190 && y < 500) {
-        m.motor1_rpm_pcm = 6142;
+        m.motor1_rpm_pcm = x;
         m.motor2_rpm_pcm = 8190;
         m.motor3_rpm_pcm = 0;
         m.motor4_rpm_pcm = 0;
