1063f86f901dcab590da3716adca2febebe12798
Alexandre Bobkov <alexandre.bobkov@gmail.com>
2025-08-06 04:24:05 -0400

    pcm

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -7,6 +7,7 @@
     Updated:    Jan 10, 2025
                 Jun 26, 2025
                 Jul 26, 2025 (ESP-IDF + MQTT + WiFi)
+                
 
     built-in LED GPIO:          10
     build-in push button GPIO:  3
