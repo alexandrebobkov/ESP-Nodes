be574ec6f2d22171674018655c09cf199ddc36a0
Alexander B <alexandre.bobkov@gmail.com>
2025-08-12 14:21:23 -0400

    GPS

--- ESP-IDF_GPS/build/CMakeFiles/git-data/grabRef.cmake
+++ ESP-IDF_GPS/build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/alex/github/ESP-Nodes/ESP-IDF_GPS/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/alex/github/ESP-Nodes/.git")
+set(GIT_DIR "/home/alex/esp/v5.4.1/esp-idf/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
