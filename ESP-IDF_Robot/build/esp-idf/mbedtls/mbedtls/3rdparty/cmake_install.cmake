6de2ba0efcdabbad40ba2b7baa348fc529fd3d10
Alexander B <alexandre.bobkov@gmail.com>
2025-09-21 01:08:49 -0400

    .

--- ESP-IDF_Robot/build/esp-idf/mbedtls/mbedtls/3rdparty/cmake_install.cmake
+++ ESP-IDF_Robot/build/esp-idf/mbedtls/mbedtls/3rdparty/cmake_install.cmake
@@ -32,15 +32,15 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
-# Set default install directory permissions.
+# Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/home/alex/.espressif/tools/riscv32-esp-elf/esp-14.2.0_20241119/riscv32-esp-elf/bin/riscv32-esp-elf-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/alex/MyProjects/ESP-Nodes/ESP-IDF_Robot/build/esp-idf/mbedtls/mbedtls/3rdparty/everest/cmake_install.cmake")
-  include("/home/alex/MyProjects/ESP-Nodes/ESP-IDF_Robot/build/esp-idf/mbedtls/mbedtls/3rdparty/p256-m/cmake_install.cmake")
+  include("/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/esp-idf/mbedtls/mbedtls/3rdparty/everest/cmake_install.cmake")
+  include("/home/alex/github/ESP-Nodes/ESP-IDF_Robot/build/esp-idf/mbedtls/mbedtls/3rdparty/p256-m/cmake_install.cmake")
 
 endif()
 
