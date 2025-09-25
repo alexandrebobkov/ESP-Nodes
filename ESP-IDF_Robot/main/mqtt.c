fbd910a3572509801b9882c397fbd8cbcd80b8ae
Alexander B <alexandre.bobkov@gmail.com>
2025-09-24 20:03:55 -0400

    .

--- ESP-IDF_Robot/main/mqtt.c
+++ ESP-IDF_Robot/main/mqtt.c
@@ -6,7 +6,7 @@
 #include "mqtt.h"
 
 //static const char* MQTT_BROKER_URI = "mqtt://10.100.50.16:1883";//74.14.210.168";//"mqtt://mqtt.techquadbit.net";
-static const char* MQTT_BROKER_URI = "mqtt://techquadbit.net";//74.14.210.168";//"mqtt://mqtt.techquadbit.net";
+static const char* MQTT_BROKER_URI = "mqtt://mqtt.techquadbit.net";//74.14.210.168";//"mqtt://mqtt.techquadbit.net";
 
 static const char* MQTT_TAG = "MQTT_Robot";
 static esp_mqtt_client_handle_t mqtt_client = NULL;
