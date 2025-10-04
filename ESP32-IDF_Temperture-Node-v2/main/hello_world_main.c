caf5125398d89d90158bf179669662b67dad42d7
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-04 01:18:11 -0400

    esp32 temperature node

--- ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
+++ ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
@@ -28,7 +28,7 @@
 #define I2C_WRITE_BIT               I2C_MASTER_WRITE
 #define I2C_READ_BIT                I2C_MASTER_READ
 
-#define BME280_SENSOR_ADDR          0x77        /*!< Slave address of the MPU9250 sensor */
+#define BME280_SENSOR_ADDR          0x76        /*!< Slave address of the MPU9250 sensor */
 
 static i2c_bus_handle_t i2c_bus = NULL;
 static bme280_handle_t sensor = NULL;
