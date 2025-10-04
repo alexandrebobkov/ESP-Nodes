0ff0ebcf38832cff26d3c534be7346a53282bc24
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-04 00:38:02 -0400

    esp32 temperature node

--- ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
+++ ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
@@ -15,8 +15,8 @@
 #include "driver/i2c.h"
 #include "bme280.h"
 
-#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_SCL_IO           22      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           21      /*!< GPIO number used for I2C master data  */
 #define I2C_MASTER_NUM              I2C_NUM_0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
 #define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
