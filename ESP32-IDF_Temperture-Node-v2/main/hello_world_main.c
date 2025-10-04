cbf032c60dab079ff3cd40ddb7dfc2a3e5986863
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-04 00:34:15 -0400

    esp32 temperature node

--- ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
+++ ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
@@ -32,6 +32,20 @@
 static bme280_handle_t sensor = NULL;
 float temperature = 0.0f;
 
+void i2c_master_init(void)
+{
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_param_config(I2C_MASTER_NUM, &conf);
+    i2c_driver_install(I2C_MASTER_NUM, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
 void app_main(void)
 {
     printf("Hello world!\n");
