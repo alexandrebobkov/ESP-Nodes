a718d9b0edea1c8b92d3864fdd3a8144b1a2280e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-04 05:28:10 -0400

    esp32 temperature node

--- ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
+++ ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
@@ -7,23 +7,8 @@
     Modified:   October 4, 2025
     
     
-    Adopted from i2c - Simple example
-
-    Simple I2C example that shows how to initialize I2C
-    as well as reading and writing from and to registers for a sensor connected over I2C.
-
-    The sensor used in this example is a MPU9250 inertial measurement unit.
-
-    For other examples please check:
-    https://github.com/espressif/esp-idf/tree/master/examples
-
-    See README.md file to get detailed usage of this example.
-
-    This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-    Unless required by applicable law or agreed to in writing, this
-    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-    CONDITIONS OF ANY KIND, either express or implied.
+    Sensor device uses BME280 to measure temperature, pressure and humidity and 
+    outputs it to the serial port and publishes values to the MQTT topics.
 */
 
 #include <stdio.h>
