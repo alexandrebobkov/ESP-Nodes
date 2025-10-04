f6213fb23ed54520b0cc7bdcafaf5ef416be9e41
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-04 01:50:53 -0400

    esp32 temperature node

--- ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
+++ ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
@@ -78,6 +78,14 @@ void app_main(void)
 
     bmp280_params_t params;
     bmp280_init_default_params(&params);
+    bmp280_t dev;
+    memset(&dev, 0, sizeof(bmp280_t));
+
+    ESP_ERROR_CHECK(bmp280_init_desc(&dev, BMP280_I2C_ADDRESS_0, 0, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO));
+    ESP_ERROR_CHECK(bmp280_init(&dev, &params));
+
+    bool bme280p = dev.id == BME280_CHIP_ID;
+    printf("BMP280: found %s\n", bme280p ? "BME280" : "BMP280");
 
     //i2c_master_init();
     //i2c_bus = i2c_bus_create(I2C_MASTER_NUM, &conf);
