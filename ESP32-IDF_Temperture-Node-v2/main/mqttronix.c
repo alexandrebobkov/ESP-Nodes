0a179702f36374a7f18bcf916eef642239f6f921
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-07 00:16:33 -0400

    mqtt

--- ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
+++ ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
@@ -11,3 +11,13 @@
 
 #include "mqtt_client.h"
 #include "mqttronix.h"
+
+void mqttronix_start(void) {
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = MQTT_BROKER_URI,
+    };
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
