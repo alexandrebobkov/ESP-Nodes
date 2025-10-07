d7fed840e66d8b5487e2ceb0eb9406f7ff2c166f
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-07 00:47:17 -0400

    mqtt

--- ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
+++ ESP32-IDF_Temperture-Node-v2/main/hello_world_main.c
@@ -5,6 +5,8 @@
     Author:     Alexander Bobkov
     Date:       October 4, 2025
     Updated:    October 7, 2025
+
+    TO-DO: add Wi-Fi connection for MQTT publishing
     
     Sensor device uses BME280 to measure temperature, pressure and humidity and 
     outputs it to the serial port and publishes values to the MQTT topics.
