299024199e635784c0ffa681c127de036286d0d9
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-09 23:26:02 -0400

    .

--- ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
+++ ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
@@ -74,6 +74,27 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     }
 }
 
+/*void sta_wifi_init(void) {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+    esp_wifi_connect();
+}*/
+
 void mqttronix_start(void) {
     esp_mqtt_client_config_t mqtt_cfg = {
         .broker.address.uri = MQTT_BROKER_URI,
