e3041ea6f73568cd9a2907876235d21acb64fdc7
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-10-10 01:14:11 -0400

    .

--- ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
+++ ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
@@ -59,7 +59,7 @@ static void mqtt_publish_task(void *arg) {
         esp_mqtt_client_publish(mqtt_client, "nodes/outdoors/foxie2/sensors/temperature", temp_str, 0, 1, 0);
         esp_mqtt_client_publish(mqtt_client, "nodes/outdoors/foxie2/sensors/humidity", humidity_str, 0, 1, 0);
         esp_mqtt_client_publish(mqtt_client, "nodes/outdoors/foxie2/sensors/pressure", pressure_str, 0, 1, 0);
-        vTaskDelay(pdMS_TO_TICKS(3000));
+        vTaskDelay(pdMS_TO_TICKS(500));
         ESP_LOGI(MQTT_TAG, "Called task to publish topic \"nodes/outdoors/foxie2/#\"");
     }
 }
