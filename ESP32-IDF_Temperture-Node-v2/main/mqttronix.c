22f8e73e4bf805a0d9087e660809487c7a311bee
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-11-01 21:25:11 -0400

    sensor node

--- ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
+++ ESP32-IDF_Temperture-Node-v2/main/mqttronix.c
@@ -3,7 +3,7 @@
 
     Author:     Alexander Bobkov
     Date:       October 4, 2025
-    Modified:   October 4, 2025
+    Modified:   November 1, 2025
 
     Built and Compiled with ESP-IDF v5.4.1
     Uses Espressif mqtt component: espressif/mqtt^1.0.0
