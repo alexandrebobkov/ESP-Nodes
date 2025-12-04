871550531cf64424ecf005a8d4dd423be3c4a9d5
Alexander Bobkov <alexandre.bobkov@gmail.com>
2025-12-03 21:27:13 -0500

    I2S Internet Radio

---
+++ ESP32-IDF_Audio-mp3/main/ESP32-IDF_Audio-mp3.c
@@ -0,0 +1,14 @@
+/*
+ * Internet stream player.
+ *
+ * Author:          Alexander Bobkov
+ * Date Created:    Dec 3, 2025
+ * Date Updated:    Dec 3, 2025
+ *
+ */
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}
