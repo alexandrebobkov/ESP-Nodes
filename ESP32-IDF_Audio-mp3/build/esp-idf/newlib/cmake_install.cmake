c710ae5f4ce274fb46d1f7b8e86ebf8aa2cb2453
Alexander B <alexandre.bobkov@gmail.com>
2025-12-06 13:45:08 -0500

    I2S

--- ESP32-IDF_Audio-mp3/build/esp-idf/newlib/cmake_install.cmake
+++ ESP32-IDF_Audio-mp3/build/esp-idf/newlib/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/abobkov/esp/v5.4.1/esp-idf/components/newlib
+# Install script for directory: /home/alex/esp/esp-idf/v5.4.1/components/newlib
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,17 +34,11 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/home/abobkov/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32-elf-objdump")
+  set(CMAKE_OBJDUMP "/home/alex/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/xtensa-esp32-elf-objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/home/abobkov/MyProjects/ESP-Nodes/ESP32-IDF_Audio-mp3/build/esp-idf/newlib/port/cmake_install.cmake")
+  include("/home/alex/github/ESP-Nodes/ESP32-IDF_Audio-mp3/build/esp-idf/newlib/port/cmake_install.cmake")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/abobkov/MyProjects/ESP-Nodes/ESP32-IDF_Audio-mp3/build/esp-idf/newlib/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
