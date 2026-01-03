396db011907930a20015e995eab49723c8ae4038
Alexander B <alexandre.bobkov@gmail.com>
2026-01-03 16:43:12 -0500

    RC

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -8,6 +8,7 @@
                 Jun 26, 2025
                 Jul 26, 2025 (ESP-IDF + MQTT + WiFi)
                 Aug 6 , 2025 Continous interpolation of joystick x- and y- values
+                Jan 3,  2026 Revised motor control logic
 
     built-in LED GPIO:          10
     build-in push button GPIO:  3
