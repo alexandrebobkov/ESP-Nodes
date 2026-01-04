22aa9f2826234e4b8bdade1c3b590ad23de5f578
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:53:51 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/ui/ui.h
+++ ESP-IDF_NovaGlide/subsystems/ui/ui.h
@@ -8,11 +8,15 @@
 #define PUSH_BTN_GPIO 8
 #define NAV_BTN_GPIO 8
 
-typedef struct {
+// Forward declaration
+typedef struct ui_system_t ui_system_t;
+
+// Struct definition
+struct ui_system_t {
     uint8_t led_state;
 
-    void (*update)(struct ui_system_t *self, TickType_t now);
-} ui_system_t;
+    void (*update)(ui_system_t *self, TickType_t now);
+};
 
 void ui_system_init(ui_system_t *sys);
 
