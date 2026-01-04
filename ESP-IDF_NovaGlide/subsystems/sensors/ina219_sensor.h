af9be3188950a811d7ea1f516fc29026cb339031
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:37:30 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/sensors/ina219_sensor.h
+++ ESP-IDF_NovaGlide/subsystems/sensors/ina219_sensor.h
@@ -1,14 +1,26 @@
-#pragma once
+#ifndef INA219_SENSOR_H
+#define INA219_SENSOR_H
+
 #include "freertos/FreeRTOS.h"
+#include "ina219.h"
 
-typedef struct ina219_system_t ina219_system_t;
+#define I2C_PORT 0
+#define I2C_ADDR 0x40
+#define I2C_SDA_GPIO 3
+#define I2C_SCL_GPIO 2
+#define SHUNT_RESISTOR_MILLI_OHM 100
 
-struct ina219_system_t {
-    float voltage;
+typedef struct {
+    float bus_voltage;
+    float shunt_voltage;
     float current;
     float power;
 
-    void (*update)(ina219_system_t *self, TickType_t now);
-};
+    ina219_t dev;
+
+    void (*update)(struct ina219_system_t *self, TickType_t now);
+} ina219_system_t;
 
 void ina219_system_init(ina219_system_t *sys);
+
+#endif
