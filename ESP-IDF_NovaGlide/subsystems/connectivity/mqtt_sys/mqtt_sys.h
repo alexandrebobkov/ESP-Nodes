75c2689a47eb9d3b01a70b7b9a91e2af58076bad
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:34:47 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/connectivity/mqtt_sys/mqtt_sys.h
+++ ESP-IDF_NovaGlide/subsystems/connectivity/mqtt_sys/mqtt_sys.h
@@ -1,16 +1,31 @@
-#pragma once
+#ifndef MQTT_SYS_H
+#define MQTT_SYS_H
+
 #include "freertos/FreeRTOS.h"
+#include "mqtt_client.h"
 
-typedef struct mqtt_system_t mqtt_system_t;
+#define MQTT_BROKER_URI "mqtt://74.14.210.168"
+#define WIFI_SSID "IoT_bots"
+#define WIFI_PASSWORD "208208208"
 
-struct mqtt_system_t {
-    float temp;
-    int pwm_1;
-    int pwm_2;
+typedef struct {
+    float temp_value;
+    float battery_voltage;
+    float sys_current;
+    float sys_power;
+    int pwm_left;
+    int pwm_right;
 
-    void (*publish_temp)(mqtt_system_t *self, float t);
-    void (*publish_pwm)(mqtt_system_t *self, int p1, int p2);
-    void (*update)(mqtt_system_t *self, TickType_t now);
-};
+    esp_mqtt_client_handle_t client;
+
+    void (*update)(struct mqtt_system_t *self, TickType_t now);
+} mqtt_system_t;
 
 void mqtt_system_init(mqtt_system_t *sys);
+void mqtt_update_temp(mqtt_system_t *sys, float temp);
+void mqtt_update_battery(mqtt_system_t *sys, float voltage);
+void mqtt_update_current(mqtt_system_t *sys, float current);
+void mqtt_update_power(mqtt_system_t *sys, float power);
+void mqtt_update_pwm(mqtt_system_t *sys, int left, int right);
+
+#endif
