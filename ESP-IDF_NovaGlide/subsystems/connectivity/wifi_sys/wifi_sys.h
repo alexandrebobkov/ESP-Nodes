bdb4f0723b2255f6571d8f90e39ec1f6ec202cb2
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:35:52 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/connectivity/wifi_sys/wifi_sys.h
+++ ESP-IDF_NovaGlide/subsystems/connectivity/wifi_sys/wifi_sys.h
@@ -1,2 +1,13 @@
-#pragma once
+#ifndef WIFI_SYS_H
+#define WIFI_SYS_H
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+
+#define WIFI_SSID "IoT_bots"
+#define WIFI_PASSWORD "208208208"
+#define ESPNOW_CHANNEL 1
+
 void wifi_system_init(void);
+
+#endif
