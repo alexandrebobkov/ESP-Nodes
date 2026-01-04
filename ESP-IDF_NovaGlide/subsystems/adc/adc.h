25c0de8df7d9807e12f0bad6cd9924c28b94bb5d
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 01:03:50 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/adc/adc.h
+++ ESP-IDF_NovaGlide/subsystems/adc/adc.h
@@ -10,8 +10,11 @@ typedef struct adc_system_t adc_system_t;
 // Struct definition
 struct adc_system_t {
     int last_reading;
+    int x_raw;  // Joystick X-axis raw value
+    int y_raw;  // Joystick Y-axis raw value
 
     void (*update)(adc_system_t *self, TickType_t now);
+    void (*read)(adc_system_t *self, int *x, int *y);
 };
 
 void adc_system_init(adc_system_t *sys);
