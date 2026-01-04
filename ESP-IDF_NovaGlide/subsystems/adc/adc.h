2a4d01931e7f981c506fca97c93ee584aee033fa
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:53:39 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/adc/adc.h
+++ ESP-IDF_NovaGlide/subsystems/adc/adc.h
@@ -1,14 +1,19 @@
-#pragma once
-#include "freertos/FreeRTOS.h"
+#ifndef ADC_H
+#define ADC_H
 
+#include "freertos/FreeRTOS.h"
+#include "esp_adc/adc_oneshot.h"
+
+// Forward declaration
 typedef struct adc_system_t adc_system_t;
 
+// Struct definition
 struct adc_system_t {
-    int x_raw;
-    int y_raw;
+    int last_reading;
 
-    void (*read)(adc_system_t *self);
     void (*update)(adc_system_t *self, TickType_t now);
 };
 
 void adc_system_init(adc_system_t *sys);
+
+#endif
