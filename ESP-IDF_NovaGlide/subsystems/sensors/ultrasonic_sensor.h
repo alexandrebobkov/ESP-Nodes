9a5dc15363be75fcba5bdb4122179021282d16f4
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:38:01 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.h
+++ ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.h
@@ -1,11 +1,21 @@
-#pragma once
+#ifndef ULTRASONIC_SENSOR_H
+#define ULTRASONIC_SENSOR_H
+
 #include "freertos/FreeRTOS.h"
+#include "ultrasonic.h"
 
-typedef struct ultrasonic_system_t ultrasonic_system_t;
+#define ULTRASONIC_TRIGGER_GPIO 4
+#define ULTRASONIC_ECHO_GPIO 5
+#define PING_TIMEOUT 6000
+#define ROUNDTRIP_CM 58.0f
 
-struct ultrasonic_system_t {
+typedef struct {
     float distance_cm;
-    void (*update)(ultrasonic_system_t *self, TickType_t now);
-};
+    ultrasonic_sensor_t sensor;
+
+    void (*update)(struct ultrasonic_system_t *self, TickType_t now);
+} ultrasonic_system_t;
 
 void ultrasonic_system_init(ultrasonic_system_t *sys);
+
+#endif
