98dd58d2296d836e33883308dfe9dba46968990e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:38:14 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.c
+++ ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.c
@@ -3,14 +3,30 @@
 
 static const char *TAG = "ULTRASONIC";
 
-static void ultra_update_impl(ultrasonic_system_t *self, TickType_t now)
-{
-    (void)now;
-    self->distance_cm = 42.0f;
+static void ultrasonic_update_impl(ultrasonic_system_t *self, TickType_t now) {
+    static TickType_t last_read = 0;
+
+    // Read every 1 second
+    if ((now - last_read) >= pdMS_TO_TICKS(1000)) {
+        uint32_t time_us;
+        esp_err_t res = ultrasonic_measure_raw(&self->sensor, PING_TIMEOUT, &time_us);
+
+        if (res == ESP_OK) {
+            self->distance_cm = (float)time_us / ROUNDTRIP_CM;
+            ESP_LOGI(TAG, "Distance: %.2f cm", self->distance_cm);
+        }
+
+        last_read = now;
+    }
 }
 
-void ultrasonic_system_init(ultrasonic_system_t *sys)
-{
+void ultrasonic_system_init(ultrasonic_system_t *sys) {
     sys->distance_cm = 0.0f;
-    sys->update = ultra_update_impl;
+    sys->sensor.trigger_gpio = ULTRASONIC_TRIGGER_GPIO;
+    sys->sensor.echo_gpio = ULTRASONIC_ECHO_GPIO;
+    sys->update = ultrasonic_update_impl;
+
+    ESP_ERROR_CHECK(ultrasonic_init(&sys->sensor));
+
+    ESP_LOGI(TAG, "Ultrasonic sensor initialized");
 }
