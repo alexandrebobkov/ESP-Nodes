ad4411b6acf02fa67be1338ae4478bcdd1701b1b
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:36:11 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/connectivity/wifi_sys/wifi_sys.c
+++ ESP-IDF_NovaGlide/subsystems/connectivity/wifi_sys/wifi_sys.c
@@ -1,28 +1,29 @@
 #include "wifi_sys.h"
-#include "esp_wifi.h"
-#include "esp_netif.h"
 #include "esp_log.h"
+#include "esp_netif.h"
 
 static const char *TAG = "WIFI_SYS";
 
-void wifi_system_init(void)
-{
+void wifi_system_init(void) {
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
-    wifi_config_t wifi_cfg = {
+    wifi_config_t wifi_config = {
         .sta = {
-            .ssid = "NovaGlides",
-            .password = "password123",
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD,
         },
     };
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_ERROR_CHECK(esp_wifi_connect());
 
-    ESP_LOGI(TAG, "WiFi initialized");
+    ESP_LOGI(TAG, "WiFi initialized and connecting to %s", WIFI_SSID);
 }
