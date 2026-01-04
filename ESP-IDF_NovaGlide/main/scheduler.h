7c7baeea4a5d51cd4abbf60e42011a18f73e0e6e
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:41:15 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/main/scheduler.h
+++ ESP-IDF_NovaGlide/main/scheduler.h
@@ -1,22 +1,29 @@
-#pragma once
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "motors.h"
+#include "adc.h"
+#include "temp_sensor.h"
+#include "ina219_sensor.h"
+#include "ultrasonic_sensor.h"
+#include "mqtt_sys.h"
+#include "espnow_sys.h"
+#include "ui.h"
 
-// Forward declarations of subsystem structs
-typedef struct motor_system_t       motor_system_t;
-typedef struct adc_system_t         adc_system_t;
-typedef struct mqtt_system_t        mqtt_system_t;
-typedef struct temp_sensor_system_t temp_sensor_system_t;
-typedef struct ina219_system_t      ina219_system_t;
-typedef struct ultrasonic_system_t  ultrasonic_system_t;
-
-typedef struct scheduler_t {
-    motor_system_t       *motors;
-    adc_system_t         *adc;
-    mqtt_system_t        *mqtt;
+typedef struct {
+    motor_system_t *motors;
+    adc_system_t *adc;
     temp_sensor_system_t *temp;
-    ina219_system_t      *ina;
-    ultrasonic_system_t  *ultra;
+    ina219_system_t *ina;
+    ultrasonic_system_t *ultra;
+    mqtt_system_t *mqtt;
+    espnow_system_t *espnow;
+    ui_system_t *ui;
 } scheduler_t;
 
 void scheduler_init(scheduler_t *sched);
 void scheduler_start(scheduler_t *sched);
+
+#endif
