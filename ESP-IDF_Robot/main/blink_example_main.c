07f721536c56590087723fc2e759c9cb25d7cd21
Alexander B <alexandre.bobkov@gmail.com>
2026-01-04 03:25:10 -0500

    Motor Logic

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -375,8 +375,8 @@ static void rc_task (void *arg) {
         update_motors_pwm (pwm_motor_1, pwm_motor_2);   // Revised motor update logic
         ESP_LOGI("x,y", "( %d, %d ) [ %d, %d] ", rc_x, rc_y, x, y);
         ESP_LOGI("pwm_motor_1, pwm_motor_2", "( %d, %d )", pwm_motor_1, pwm_motor_2);
-        vTaskDelay (100 / portTICK_PERIOD_MS);  // Determines responsiveness
-        //vTaskDelay (1000 / portTICK_PERIOD_MS);
+        //vTaskDelay (100 / portTICK_PERIOD_MS);  // Determines responsiveness
+        vTaskDelay (1000 / portTICK_PERIOD_MS);
     }
 }
 static void display_xy() {
