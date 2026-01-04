adfa57ba7ea77bee394d5a6df5e8163119ca5f40
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-03 22:58:34 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/build/CMakeFiles/3.31.6/CMakeSystem.cmake
+++ ESP-IDF_NovaGlide/build/CMakeFiles/3.31.6/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.17.0-8-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/abobkov/esp/v5.4.1/esp-idf/tools/cmake/toolchain-esp32c3.cmake")
+include("/home/abobkov/esp/v5.4.1/esp-idf/tools/cmake/toolchain-esp32.cmake")
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
