3c5687a90f1e5e3630301b9a78a60f8eeb665d1a
Alexander B <alexandre.bobkov@gmail.com>
2026-01-04 03:18:44 -0500

    Motor Logic

--- ESP-IDF_Robot/main/blink_example_main.c
+++ ESP-IDF_Robot/main/blink_example_main.c
@@ -370,8 +370,8 @@ void joystick_mix (int X_raw, int Y_raw, int *pwm_a, int *pwm_b) {
 // Task function to read joystick values and update motors rotation speeds.
 static void rc_task (void *arg) {
     while (true) {
-        update_pwm (rc_x, rc_y);     // Orginal motor update logic
-        //joystick_mix (rc_y, rc_x, &pwm_motor_1, &pwm_motor_2);
+        //update_pwm (rc_x, rc_y);     // Orginal motor update logic
+        joystick_mix (rc_y, rc_x, &pwm_motor_1, &pwm_motor_2);
         update_motors_pwm (pwm_motor_1, pwm_motor_2);   // Revised motor update logic
         ESP_LOGI("x,y", "( %d, %d ) [ %d, %d] ", rc_x, rc_y, x, y);
         vTaskDelay (100 / portTICK_PERIOD_MS);  // Determines responsiveness
