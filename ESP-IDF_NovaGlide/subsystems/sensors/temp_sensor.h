b496c6a01827c092d2a4504fc9b6362da74bd7e0
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:52:54 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/sensors/temp_sensor.h
+++ ESP-IDF_NovaGlide/subsystems/sensors/temp_sensor.h
@@ -4,12 +4,16 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/temperature_sensor.h"
 
-typedef struct {
+// Forward declaration
+typedef struct temp_sensor_system_t temp_sensor_system_t;
+
+// Struct definition
+struct temp_sensor_system_t {
     float temperature;
     temperature_sensor_handle_t handle;
 
-    void (*update)(struct temp_sensor_system_t *self, TickType_t now);
-} temp_sensor_system_t;
+    void (*update)(temp_sensor_system_t *self, TickType_t now);
+};
 
 void temp_sensor_system_init(temp_sensor_system_t *sys);
 
