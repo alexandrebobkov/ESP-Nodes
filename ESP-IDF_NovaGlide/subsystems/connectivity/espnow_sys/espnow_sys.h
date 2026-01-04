65b62c404b0deff102cb588343f8177c24b55054
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-04 00:36:31 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/connectivity/espnow_sys/espnow_sys.h
+++ ESP-IDF_NovaGlide/subsystems/connectivity/espnow_sys/espnow_sys.h
@@ -1,17 +1,27 @@
-#pragma once
+#ifndef ESPNOW_SYS_H
+#define ESPNOW_SYS_H
+
 #include "freertos/FreeRTOS.h"
 #include "esp_now.h"
+#include "esp_wifi.h"
 
-typedef struct espnow_system_t espnow_system_t;
+typedef struct {
+    int x_axis;
+    int y_axis;
+    int motor1_rpm_pcm;
+    int motor2_rpm_pcm;
+    int motor3_rpm_pcm;
+    int motor4_rpm_pcm;
+} sensors_data_t;
 
-struct espnow_system_t {
-    // Last received data (you can expand this later)
-    uint8_t last_data[32];
+typedef struct {
+    sensors_data_t last_data;
     int last_len;
 
-    // API
-    void (*send)(espnow_system_t *self, const uint8_t *data, int len);
-    void (*update)(espnow_system_t *self, TickType_t now);
-};
+    void (*send)(struct espnow_system_t *self, const uint8_t *data, int len);
+    void (*update)(struct espnow_system_t *self, TickType_t now);
+} espnow_system_t;
 
 void espnow_system_init(espnow_system_t *sys);
+
+#endif
