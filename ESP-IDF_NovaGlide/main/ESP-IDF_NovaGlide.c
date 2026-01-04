02ab4d294141e4f90cb5e2b25c8be14564272192
Alexander Bobkov <alexandre.bobkov@gmail.com>
2026-01-03 23:14:09 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/main/ESP-IDF_NovaGlide.c
+++ ESP-IDF_NovaGlide/main/ESP-IDF_NovaGlide.c
@@ -18,36 +18,35 @@ void app_main(void)
     system_init();
 
     // --- Subsystem instances ---
-    //static motor_system_t       motors;
-    //static adc_system_t         adc;
+    static motor_system_t       motors;
+    static adc_system_t         adc;
     static temp_sensor_system_t temp;
-    //static ina219_system_t      ina;
-    //static ultrasonic_system_t  ultra;
-    //static mqtt_system_t        mqtt;
+    static ina219_system_t      ina;
+    static ultrasonic_system_t  ultra;
+    static mqtt_system_t        mqtt;
 
     // --- Initialize subsystems (you will add these one by one) ---
-    //motor_system_init(&motors);
-    //adc_system_init(&adc);
+    motor_system_init(&motors);
+    adc_system_init(&adc);
     temp_sensor_system_init(&temp);
-    //ina219_system_init(&ina);
-    //ultrasonic_system_init(&ultra);
+    ina219_system_init(&ina);
+    ultrasonic_system_init(&ultra);
 
     wifi_system_init();                 // stateless
-    //espnow_system_init(&adc, &motors);  // passes references
-    //mqtt_system_init(&mqtt);
-
+    espnow_system_init(&adc, &motors);  // passes references
+    mqtt_system_init(&mqtt);
     //ui_led_init();
     //ui_buttons_init();
 
     // --- Scheduler wiring ---
-    //static scheduler_t sched = {
-    //    .motors = &motors,
-    //    .adc    = &adc,
-    //    .temp   = &temp,
-    //    .ina    = &ina,
-    //    .ultra  = &ultra,
-    //    .mqtt   = &mqtt
-    //};
+    static scheduler_t sched = {
+        .motors = &motors,
+        .adc    = &adc,
+        .temp   = &temp,
+        .ina    = &ina,
+        .ultra  = &ultra,
+        .mqtt   = &mqtt
+    };
 
     scheduler_init(&sched);
     scheduler_start(&sched);
