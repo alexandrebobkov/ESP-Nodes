8c7eb932be64e584ab71877b75c5af25803691ea
Alexander B <alexandre.bobkov@gmail.com>
2026-01-04 16:26:15 -0500

    NovaGlide

---
+++ ESP-IDF_NovaGlide/subsystems/ui/dashboard.h
@@ -0,0 +1,20 @@
+// dashboard.h
+#ifndef DASHBOARD_H
+#define DASHBOARD_H
+
+#include "freertos/FreeRTOS.h"
+#include "motors.h"
+#include "espnow_sys.h"
+#include "temp_sensor.h"
+#include "ina219_sensor.h"
+
+typedef struct {
+    motor_system_t *motors;
+    espnow_system_t *espnow;
+    temp_sensor_system_t *temp;
+    ina219_system_t *ina;
+} dashboard_context_t;
+
+void dashboard_task_start(dashboard_context_t *ctx);
+
+#endif
