d314a5603687b9830f2d90ec8c02480c9b378d01
Alexander B <alexandre.bobkov@gmail.com>
2026-01-04 04:50:10 -0500

    Motor Logic

--- ESP-IDF_Robot/main/rc.h
+++ ESP-IDF_Robot/main/rc.h
@@ -217,11 +217,19 @@ static void update_pwm (int rc_x, int rc_y) {
 
 static void update_motors_pwm (int pwm_motor_1, int pwm_motor_2) {
 
-    /* 1. DEAD ZONE / STAND */
+    /* DEAD ZONE / STAND */
     if (pwm_motor_1 >= -500 && pwm_motor_1 <= 1000 && pwm_motor_2 >= -500 && pwm_motor_2 <= 1000)
     {
         m.motor1_rpm_pcm = 0;
         m.motor2_rpm_pcm = 0;
+        m.motor3_rpm_pcm = 0;
+        m.motor4_rpm_pcm = 0;
+    }
+    // FORWARD
+    else if (pwm_motor_1 > 1000 && pwm_motor_2 > 1000)
+    {
+        m.motor1_rpm_pcm = pwm_motor_1;   // left, forward
+        m.motor2_rpm_pcm = pwm_motor_2;   // right, forward
         m.motor3_rpm_pcm = 0;
         m.motor4_rpm_pcm = 0;
     }
