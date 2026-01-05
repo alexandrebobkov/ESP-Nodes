ad4414fd930611876020a8ed85ea361ab799f8ff
Alexander B <alexandre.bobkov@gmail.com>
2026-01-05 01:59:56 -0500

    NovaGlide ultrasonic + i2c mutex

--- ESP-IDF_NovaGlide/main/ESP-IDF_NovaGlide.c
+++ ESP-IDF_NovaGlide/main/ESP-IDF_NovaGlide.c
@@ -87,7 +87,7 @@ void app_main(void)
     adc_system_init(&adc);
     temp_sensor_system_init(&temp);
     ina219_system_init(&ina);
-    //ultrasonic_system_init(&ultra);
+    ultrasonic_system_init(&ultra);
     espnow_system_init(&espnow);
     mqtt_system_init(&mqtt);
     ui_system_init(&ui);
