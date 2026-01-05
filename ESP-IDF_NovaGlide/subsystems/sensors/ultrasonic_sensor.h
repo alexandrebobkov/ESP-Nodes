6a271c9e5d9e33f9fa33371273a77c4eb6ea0069
Alexander B <alexandre.bobkov@gmail.com>
2026-01-04 20:45:40 -0500

    NovaGlide ultrasonic + i2c

--- ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.h
+++ ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.h
@@ -2,22 +2,11 @@
 #define ULTRASONIC_SENSOR_H
 
 #include "freertos/FreeRTOS.h"
-#include "ultrasonic.h"
 #include "driver/i2c_master.h"
 
-#define ULTRASONIC_TRIGGER_GPIO 4
-#define ULTRASONIC_ECHO_GPIO 5
-#define PING_TIMEOUT 6000
-#define ROUNDTRIP_CM 58.0f
-
-#define I2C_PORT            I2C_NUM_0
-#define I2C_SDA_PIN         21
-#define I2C_SCL_PIN         22
-#define I2C_FREQ_HZ         100000
-#define ULTRASONIC_I2C_ADDR 0x57   // HC-SR04 I2C mode
-#define ULTRASONIC_I2C_SPEED_HZ 100000
-
-
+// I2C Configuration for HC-SR04 I2C module
+#define ULTRASONIC_I2C_ADDR         0x57    // Common I2C address for HC-SR04 I2C modules
+#define ULTRASONIC_I2C_SPEED_HZ     100000  // 100kHz
 
 // Forward declaration
 typedef struct ultrasonic_system_t ultrasonic_system_t;
@@ -25,8 +14,7 @@ typedef struct ultrasonic_system_t ultrasonic_system_t;
 // Struct definition
 struct ultrasonic_system_t {
     float distance_cm;
-    i2c_master_dev_handle_t dev; // I2C device handle
-    ultrasonic_sensor_t sensor;
+    i2c_master_dev_handle_t dev;  // I2C device handle
 
     void (*update)(ultrasonic_system_t *self, TickType_t now);
 };
