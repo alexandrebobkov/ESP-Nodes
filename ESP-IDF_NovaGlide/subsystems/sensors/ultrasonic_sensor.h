7cbd0dc9c9bedeb1f8db59b79d2f0cb4173567b8
Alexander B <alexandre.bobkov@gmail.com>
2026-01-05 03:10:49 -0500

    NovaGlide ultrasonic + i2c mutex

--- ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.h
+++ ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_sensor.h
@@ -1,27 +1,17 @@
 #ifndef ULTRASONIC_SENSOR_H
 #define ULTRASONIC_SENSOR_H
 
-#include "freertos/FreeRTOS.h"
 #include "driver/i2c_master.h"
+#include "freertos/FreeRTOS.h"
 
-// I2C Configuration for HC-SR04 I2C module
-#define ULTRASONIC_I2C_ADDR         0x57    // Common I2C address for HC-SR04 I2C modules
-#define ULTRASONIC_I2C_SPEED_HZ     100000  // 100kHz
+#define ULTRASONIC_I2C_ADDR 0x57
 
-// Forward declaration
-typedef struct ultrasonic_system_t ultrasonic_system_t;
-
-// Struct definition
-struct ultrasonic_system_t {
+typedef struct ultrasonic_system_t {
     float distance_cm;
-    i2c_master_dev_handle_t dev;  // I2C device handle
-
-    void (*update)(ultrasonic_system_t *self, TickType_t now);
-};
+    i2c_master_dev_handle_t dev;
+    void (*update)(struct ultrasonic_system_t *self, TickType_t now);
+} ultrasonic_system_t;
 
 void ultrasonic_system_init(ultrasonic_system_t *sys);
-void ultrasonic_system_init(ultrasonic_system_t *ultra);
-void ultrasonic_probe_registers(ultrasonic_system_t *ultra);
-
 
 #endif
