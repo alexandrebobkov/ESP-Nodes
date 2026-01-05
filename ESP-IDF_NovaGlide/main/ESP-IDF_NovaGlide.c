1d4a0fbdd38bdb4cf9fac87019fb0e33156a3603
Alexander B <alexandre.bobkov@gmail.com>
2026-01-05 02:34:27 -0500

    NovaGlide ultrasonic + i2c mutex

--- ESP-IDF_NovaGlide/main/ESP-IDF_NovaGlide.c
+++ ESP-IDF_NovaGlide/main/ESP-IDF_NovaGlide.c
@@ -64,7 +64,7 @@ void app_main(void)
     system_init();
 
     // Set log levels to WARNING or ERROR only
-    //esp_log_level_set("*", ESP_LOG_WARN);//ERROR);//_WARN);  // Global default
+    //esp_log_level_set("*", ESP_LOG_/ERROR);//_WARN);  // Global default
     //esp_log_level_set("DASHBOARD", ESP_LOG_INFO);  // Allow dashboard
 
     // Subsystem instances
