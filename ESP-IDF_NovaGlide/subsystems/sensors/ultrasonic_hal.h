c34048649e63af76b17599dd84ea9f02e71fa1e6
Alexander B <alexandre.bobkov@gmail.com>
2026-01-05 03:35:38 -0500

    NovaGlide ultrasonic HAL

--- ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_hal.h
+++ ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_hal.h
@@ -9,16 +9,13 @@
 
 typedef struct ultrasonic_system_t ultrasonic_system_t;
 
-struct ultrasonic_hal_t {
+struct ultrasonic_system_t {
     gpio_num_t trig_pin;
     gpio_num_t echo_pin;
-
     rmt_channel_handle_t rmt_tx;
     rmt_channel_handle_t rmt_rx;
-
     float distance_cm;
-
-    void (*update)(ultrasonic_hal_t *self, TickType_t now);
+    void (*update)(struct ultrasonic_system_t *self, TickType_t now);
 };
 
 void ultrasonic_hal_init(ultrasonic_hal_t *ultra,
