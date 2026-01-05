8701ca50c13e1cf1326622c9fa79002a04e5ad93
Alexander B <alexandre.bobkov@gmail.com>
2026-01-05 08:42:51 -0500

    NovaGlide

--- ESP-IDF_NovaGlide/subsystems/controls/joystick.c
+++ ESP-IDF_NovaGlide/subsystems/controls/joystick.c
@@ -6,8 +6,12 @@ static float clampf(float val, float min, float max) {
 
 void joystick_mix(int X_raw, int Y_raw, int *pwm_left, int *pwm_right) {
     // 1. Normalize joystick to [-1 .. +1]
-    float x = (float)(X_raw - 1020) / 1020.0f;
-    float y = (float)(Y_raw - 1020) / 1020.0f;
+    //float x = (float)(X_raw - 1020) / 1020.0f;
+    //float y = (float)(Y_raw - 1020) / 1020.0f;
+
+    float x = (float)(X_raw - 63635456) / 63635456.0f;
+    float y = (float)(Y_raw - 66912256) / 66912256.0f;
+
 
     // 2. Steering gain for smooth arcs
     const float k = 0.5f;
