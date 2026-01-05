8d9942f58898bf0661d2e7d7cc72ce500f18f7d5
Alexander B <alexandre.bobkov@gmail.com>
2026-01-05 03:07:48 -0500

    NovaGlide ultrasonic + i2c mutex

---
+++ ESP-IDF_NovaGlide/subsystems/sensors/ultrasonic_hal.h
@@ -0,0 +1,28 @@
+#ifndef ULTRASONIC_HAL_H
+#define ULTRASONIC_HAL_H
+
+#include "driver/gpio.h"
+#include "driver/rmt_tx.h"
+#include "driver/rmt_rx.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+typedef struct ultrasonic_hal_t ultrasonic_hal_t;
+
+struct ultrasonic_hal_t {
+    gpio_num_t trig_pin;
+    gpio_num_t echo_pin;
+
+    rmt_channel_handle_t rmt_tx;
+    rmt_channel_handle_t rmt_rx;
+
+    float distance_cm;
+
+    void (*update)(ultrasonic_hal_t *self, TickType_t now);
+};
+
+void ultrasonic_hal_init(ultrasonic_hal_t *ultra,
+                         gpio_num_t trig_pin,
+                         gpio_num_t echo_pin);
+
+#endif
